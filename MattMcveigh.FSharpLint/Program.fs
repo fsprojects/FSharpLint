efe54ebdf6fd95fd6181a6e101243f2921b2a2cb
Matt <matt@mattmcveigh.co.uk>
2014-02-03 02:06:53 +0000

    Started having a look at the ast

--- MattMcveigh.FSharpLint/Program.fs
+++ MattMcveigh.FSharpLint/Program.fs
@@ -20,15 +20,76 @@ namespace MattMcveigh.FSharpLint
 
 module Program =
 
+    open System
+    open System.Text.RegularExpressions
+    open Microsoft.FSharp.Compiler.Range
+    open Microsoft.FSharp.Compiler.Ast
+    open Microsoft.FSharp.Compiler.SourceCodeServices
     open Tokeniser
 
+    let checker = InteractiveChecker.Create()
+
+    let getUntypedTree (file, input) = 
+        let projOptions = checker.GetProjectOptionsFromScript(file, input)
+        let parseFileResults = checker.ParseFileInProject(file, input, projOptions)
+        match parseFileResults.ParseTree with
+        | Some tree -> tree
+        | None -> failwith "Something went wrong during parsing!"
+
+    let isPascalCase str = Regex.Match(str, @"^[A-Z]([a-z]|[A-Z]|\d)*").Success
+
+    let isCamelCase str = Regex.Match(str, @"^[a-z]([a-z]|[A-Z]|\d)*").Success
+
+    let errorInfoLine (range:range) (input:string) =
+        let errorenousLine = input.Split([|'\n'|]).[range.StartLine]
+        let firstLine = String.Format("Error in file {0} on line {1} starting at column {2}", range.FileName, range.StartLine, range.StartColumn)
+        let highlightColumnLine = 
+            errorenousLine 
+                |> Seq.mapi (fun i x -> if i = range.StartColumn then "^" else " ")
+                |> Seq.reduce (+)
+
+        firstLine + Environment.NewLine + errorenousLine + Environment.NewLine + highlightColumnLine
+
+    let visitModulesAndNamespaces modulesOrNamespaces input =
+        let isValidName (identifier:Ident) = isPascalCase identifier.idText
+
+        let toError isModule (identifier:Ident) range =
+            let firstLine = errorInfoLine range input
+            let moduleOrNamespace = if isModule then "module" else "namespace"
+            let secondLine = String.Format("Expected {0} {1} to be PascalCase", moduleOrNamespace, identifier.idText)
+            firstLine + Environment.NewLine + secondLine
+
+        let getErrors moduleOrNamespace =
+            let (SynModuleOrNamespace(longIndentifier, isModule, declarations, _, _, _, range)) = moduleOrNamespace
+
+            longIndentifier |> List.choose (fun identifier ->
+                if isValidName identifier then
+                    None
+                else
+                    Some(toError isModule identifier range))
+
+        modulesOrNamespaces |> List.map getErrors
+        
     [<EntryPoint>]
     let main argv = 
-        let tokens = tokenise "" [ "let dog = 7 + 8"; "let meow = function | x -> ()" ]
+        let input = """
+          namespace MattMcveigh.dogharpLint
 
-        tokens |> List.iteri (fun i list -> 
-            System.Console.Write("\n\nLine " + (i + 1).ToString() + ":\n")
-            List.iter (fun tok -> System.Console.Write(tok.ToString() + " ")) list)
+          module Program =
+            let foo() = 
+              let msg = "Hello world"
+              if true then 
+                printfn "%s" msg """
+
+        let errors = 
+            match getUntypedTree("/home/user/Dog.test.fsx", input)  with
+            | ParsedInput.ImplFile(implFile) ->
+                // Extract declarations and walk over them
+                let (ParsedImplFileInput(fn, script, name, _, _, modules, _)) = implFile
+                visitModulesAndNamespaces modules input
+            | _ -> failwith "F# Interface file (*.fsi) not supported."
+
+        errors |> List.iter System.Console.WriteLine
 
         System.Console.ReadKey() |> ignore
 
