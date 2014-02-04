3a2ae2a5e0a1b345273292ec9b1343e4c5af14fb
Matt <matt@mattmcveigh.co.uk>
2014-02-04 00:04:15 +0000

    Some more playing around with the ast

--- MattMcveigh.FSharpLint/Program.fs
+++ MattMcveigh.FSharpLint/Program.fs
@@ -50,6 +50,140 @@ module Program =
 
         firstLine + Environment.NewLine + errorenousLine + Environment.NewLine + highlightColumnLine
 
+    let visitType = function
+        | SynType.Fun(_, _, _) -> ()
+        | _ -> ()
+
+    let rec visitPattern = function
+        | SynPat.Named(pat, name, _, _, _) ->
+            visitPattern pat
+            printfn "%s" name.idText
+        | SynPat.LongIdent(LongIdentWithDots(ident, _), _, _, _, _, _) ->
+            let names = String.concat "." [ for i in ident -> i.idText ]
+            printfn "%s" names
+        | SynPat.Const(_,m) -> ()
+        | SynPat.Or (pattern1, pattern2, m) -> 
+            visitPattern pattern1
+            visitPattern pattern2
+        | SynPat.Ands (patterns,m)
+        | SynPat.ArrayOrList(_,patterns,m)
+        | SynPat.Tuple (patterns,m) -> 
+            for pattern in patterns do visitPattern pattern
+        | SynPat.Typed(pattern, synType, _) -> 
+            visitPattern pattern
+            visitType synType
+        | SynPat.Attrib(_,_,m) 
+        | SynPat.Record (_,m) 
+        | SynPat.IsInst (_,m) 
+        | SynPat.QuoteExpr (_,m)
+        | SynPat.InstanceMember(_,_,_,_,m) -> ()
+        | SynPat.OptionalVal(identifier,m) -> printfn "%s" identifier.idText
+        | SynPat.Paren(_,m) -> ()
+        | _ -> ()
+
+    let rec visitExpression = function
+        | SynExpr.IfThenElse(cond, trueBranch, falseBranchOpt, _, _, _, _) ->
+            visitExpression cond
+            visitExpression trueBranch
+            falseBranchOpt |> Option.iter visitExpression 
+        | SynExpr.LetOrUse(_, _, bindings, body, _) ->
+            for binding in bindings do
+                let (Binding(_, _, _, _, _, _, _, pat, _, init, _, _)) = binding
+                visitPattern pat 
+                visitExpression init
+                visitExpression body
+        | SynExpr.Paren(_,_,_,m) 
+        | SynExpr.Quote(_,_,_,_,m) 
+        | SynExpr.Const(_,m) 
+        | SynExpr.Typed (_,_,m)
+        | SynExpr.Tuple (_,_,m)
+        | SynExpr.ArrayOrList (_,_,m)
+        | SynExpr.Record (_,_,_,m)
+        | SynExpr.New (_,_,_,m)
+        | SynExpr.ObjExpr (_,_,_,_,_,m)
+        | SynExpr.While (_,_,_,m)
+        | SynExpr.For (_,_,_,_,_,_,m)
+        | SynExpr.ForEach (_,_,_,_,_,_,m)
+        | SynExpr.CompExpr (_,_,_,m)
+        | SynExpr.ArrayOrListOfSeqExpr (_,_,m)
+        | SynExpr.Lambda (_,_,_,_,m)
+        | SynExpr.Match (_,_,_,_,m)
+        | SynExpr.MatchLambda (_,_,_,_,m)
+        | SynExpr.Do (_,m)
+        | SynExpr.Assert (_,m)
+        | SynExpr.App (_,_,_,_,m)
+        | SynExpr.TypeApp (_,_,_,_,_,_,m)
+        | SynExpr.TryWith (_,_,_,_,m,_,_)
+        | SynExpr.TryFinally (_,_,m,_,_)
+        | SynExpr.Sequential (_,_,_,_,m)
+        | SynExpr.ArbitraryAfterError(_,m)
+        | SynExpr.FromParseError (_,m) 
+        | SynExpr.DiscardAfterMissingQualificationAfterDot (_,m) 
+        | SynExpr.LongIdent (_,_,_,m)
+        | SynExpr.LongIdentSet (_,_,m)
+        | SynExpr.NamedIndexedPropertySet (_,_,_,m)
+        | SynExpr.DotIndexedGet (_,_,_,m)
+        | SynExpr.DotIndexedSet (_,_,_,_,_,m)
+        | SynExpr.DotGet (_,_,_,m)
+        | SynExpr.DotSet (_,_,_,m)
+        | SynExpr.DotNamedIndexedPropertySet (_,_,_,_,m)
+        | SynExpr.LibraryOnlyUnionCaseFieldGet (_,_,_,m)
+        | SynExpr.LibraryOnlyUnionCaseFieldSet (_,_,_,_,m)
+        | SynExpr.LibraryOnlyILAssembly (_,_,_,_,m)
+        | SynExpr.LibraryOnlyStaticOptimization (_,_,_,m)
+        | SynExpr.TypeTest (_,_,m)
+        | SynExpr.Upcast (_,_,m)
+        | SynExpr.AddressOf (_,_,_,m)
+        | SynExpr.Downcast (_,_,m)
+        | SynExpr.JoinIn (_,_,_,m)
+        | SynExpr.InferredUpcast (_,m)
+        | SynExpr.InferredDowncast (_,m)
+        | SynExpr.Null m
+        | SynExpr.Lazy (_, m)
+        | SynExpr.TraitCall(_,_,_,m)
+        | SynExpr.ImplicitZero (m)
+        | SynExpr.YieldOrReturn (_,_,m)
+        | SynExpr.YieldOrReturnFrom (_,_,m)
+        | SynExpr.LetOrUseBang  (_,_,_,_,_,_,m)
+        | SynExpr.DoBang  (_,m) -> ()
+        | SynExpr.Ident id -> ()
+
+    let visitSimpleTypeDef = function
+    | SynTypeDefnSimpleRepr.Union(_, unionCases, _) -> 
+        for unionCase in unionCases do
+            match unionCase with
+            | SynUnionCase.UnionCase(_, identifier, _, _, _, _) -> ()
+    | SynTypeDefnSimpleRepr.Record(_, fields, _) -> 
+        for field in fields do
+            match field with
+            | SynField.Field(_, _, identifier, _, _, _, _, _) -> ()
+    | _ -> ()
+
+    let visitTypeDef = function
+    | SynTypeDefn.TypeDefn(a, typeDefinition, members, _) -> 
+        match typeDefinition with
+        | SynTypeDefnRepr.Simple(typedefintion, _) -> visitSimpleTypeDef typedefintion
+        | _ -> ()
+
+    let rec visitDeclarations decls = 
+        for declaration in decls do
+            match declaration with
+            | SynModuleDecl.Let(_, bindings, _) ->
+                for binding in bindings do
+                    let (Binding(_, _, _, _, _, _, _, pattern, _, body, _, _)) = binding
+                    visitPattern pattern 
+                    visitExpression body         
+            | SynModuleDecl.NestedModule(_, declrs, _, _) -> visitDeclarations declrs
+            | SynModuleDecl.ModuleAbbrev(identifier, longIdentifier, _) -> ()
+            | SynModuleDecl.DoExpr(_, expression, _) -> ()
+            | SynModuleDecl.Types(types, _) -> 
+                for typeDefinition in types do visitTypeDef typeDefinition
+            | SynModuleDecl.Exception(_, _) -> ()
+            | SynModuleDecl.NamespaceFragment(SynModuleOrNamespace(_,_,_,_,_,_,_)) -> ()
+            | SynModuleDecl.Attributes(_, _) -> ()
+            | SynModuleDecl.HashDirective(_, _) -> ()
+            | SynModuleDecl.Open(_, _) -> ()
+
     let visitModulesAndNamespaces modulesOrNamespaces input =
         let isValidName (identifier:Ident) = isPascalCase identifier.idText
 
@@ -61,6 +195,8 @@ module Program =
 
         let getErrors moduleOrNamespace =
             let (SynModuleOrNamespace(longIndentifier, isModule, declarations, _, _, _, range)) = moduleOrNamespace
+
+            visitDeclarations declarations
 
             longIndentifier |> List.choose (fun identifier ->
                 if isValidName identifier then
@@ -76,6 +212,12 @@ module Program =
           namespace MattMcveigh.dogharpLint
 
           module Program =
+            type dog = { cat: int; poo: string }
+
+            type guinea =
+            | Some
+            | None
+
             let foo() = 
               let msg = "Hello world"
               if true then 
