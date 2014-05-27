d26a4cd9034a0add5f0254c106a963979d71ec12
Matt <matt@mattmcveigh.co.uk>
2014-05-27 23:24:26 +0100

    check nunit isn't already installed

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ cls
 if not exist packages\FAKE\tools\Fake.exe (
   .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
 )
-if not exist tools\Nunit\nunit-console.exe (
+if not exist tools\NUnit.Runners\tools\nunit-console.exe (
   .nuget\nuget.exe install NUnit.Runners -OutputDirectory tools -ExcludeVersion
 )
 packages\FAKE\tools\FAKE.exe build.fsx %*
