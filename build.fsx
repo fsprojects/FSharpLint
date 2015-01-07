4667ea515457d9769970ac1d5e96b3e771768caa
Matt <matt@mattmcveigh.co.uk>
2015-01-07 13:36:25 +0000

    Fix build on mono

--- build.fsx
+++ build.fsx
@@ -127,8 +127,7 @@ Target "CreatePackage" (fun _ ->
         "nugetpackage/FSharpLint.nuspec"
 )
 
-#r @"tools/FSharpLint.0.1.12/FSharpLint.Application.dll"
-#r @"tools/FSharpLint.0.1.12/FSharpLint.Framework.dll"
+#I @"tools/FSharpLint.0.1.12/"
 #r @"tools/FSharpLint.0.1.12/FSharpLint.FAKE.dll"
 open FSharpLint.FAKE
 
