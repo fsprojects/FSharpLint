3a52c737d83d4a2a2b97c40f2e2433801005890c
Matt <matt@mattmcveigh.co.uk>
2015-02-15 18:35:04 +0000

    update acceptance test

--- tests/FSharpLint.FunctionalTest/TestConsoleApplication.fs
+++ tests/FSharpLint.FunctionalTest/TestConsoleApplication.fs
@@ -118,7 +118,7 @@ module Tests =
 
             let output = runConsoleApp arguments
 
-            Assert.IsTrue(output.Contains("Could not find file"), sprintf "Output:\n%s" output)
+            Assert.IsTrue(output.Contains("Could not find file") || output.Contains("not found"), sprintf "Output:\n%s" output)
 
         [<Test>]
         member this.FunctionalTestConsoleApplication() = 
