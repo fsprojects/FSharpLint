1b52214f983b72881331078ab313050b436bc82f
Matt <matt@mattmcveigh.co.uk>
2016-06-12 15:35:41 +0100

    Simplify function composition rule

--- src/FSharpLint.Rules/FunctionReimplementation.fs
+++ src/FSharpLint.Rules/FunctionReimplementation.fs
@@ -55,109 +55,26 @@ module FunctionReimplementation =
         | SynSimplePats.Typed(simplePatterns, _, _) -> 
             getLambdaParamIdent simplePatterns
 
-    let getLambdaParametersAndExpression lambda =
-        let rec getLambdaParametersAndExpression parameters = function
-            | SynExpr.Lambda(_, _, parameter, expression, _) when simplePatternsLength parameter > 0 ->
-                let ident = getLambdaParamIdent parameter
-                getLambdaParametersAndExpression (ident :: parameters) expression
-            // Unit argument
-            | SynExpr.Lambda(_, _, _, expression, _) -> ([], expression)
-            | expression -> (parameters, expression)
-
-        getLambdaParametersAndExpression [] lambda
-        
-    type IdentifierInExpression =
-        | Shadowed
-        | Found
-        | NotFound
-
-    let lambdaArgumentsShadowIdent (ident:Ident) = function
-        | SynExpr.Lambda(_) as lambda -> 
-            let arguments, _ = getLambdaParametersAndExpression lambda
-            arguments |> List.exists (Option.exists (fun x -> x.idText = ident.idText))
-        | _ -> false
-
-    let patternShadowsIdentifier (identifier:Ident) =
-        let rec patternShadowsIdentifier found = function
-            | Pattern(SynPat.Named(_, ident, _, _, _) | SynPat.OptionalVal(ident, _)) when ident.idText = identifier.idText -> true
-            | node -> 
-                if not found then
-                    traverseNode node |> List.map (fun x -> x.AstNode) |> List.fold patternShadowsIdentifier false
-                else 
-                    found
-
-        patternShadowsIdentifier false
-
-    let rec expressionReferencesIdentifier (ident:Ident) expression =
-        let rec expressionReferencesIdentifier isFound expr = 
-            match (isFound, expr) with
-            | NotFound, Expression(SynExpr.Ident(exprIdent))
-            | NotFound, Expression(SynExpr.LongIdent(_, LongIdentWithDots.LongIdentWithDots(exprIdent::_, _), _, _))
-                    when exprIdent.idText = ident.idText ->
-                Found
-            | NotFound, Expression(SynExpr.Lambda(_) as lambda) when lambdaArgumentsShadowIdent ident lambda -> 
-                NotFound
-            | NotFound, Expression(SynExpr.LetOrUseBang(_, _, _, pattern, expression, bodyExpr, _))
-            | NotFound, Expression(SynExpr.LetOrUse(_, _, [SynBinding.Binding(_, _, _, _, _, _, _, pattern, _, expression, _, _)], bodyExpr, _)) -> 
-                match expressionReferencesIdentifier NotFound (Expression(expression)) with
-                | Found -> 
-                    Found
-                | NotFound | Shadowed when patternShadowsIdentifier ident (Pattern(pattern)) -> 
-                    Shadowed
-                | _ -> 
-                    match expressionReferencesIdentifier NotFound (Expression(bodyExpr)) with
-                    | Shadowed -> NotFound
-                    | isFound -> isFound
-            | NotFound, Expression(SynExpr.For(_, iteratorIdent, startExpr, _, finishExpr, _, _)) when iteratorIdent.idText = ident.idText -> 
-                match expressionReferencesIdentifier NotFound (Expression(startExpr)) with
-                | Found -> 
-                    Found
-                | _ -> 
-                    match expressionReferencesIdentifier NotFound (Expression(finishExpr)) with
-                    | Found -> Found
-                    | _ -> NotFound
-            | NotFound, Expression(SynExpr.ForEach(_, _, _, pattern, expr, _, _)) when patternShadowsIdentifier ident (Pattern(pattern)) -> 
-                match expressionReferencesIdentifier NotFound (Expression(expr)) with
-                | Found -> Found
-                | _ -> NotFound
-            | NotFound, Match(SynMatchClause.Clause(pattern, _, _, _, _)) when patternShadowsIdentifier ident (Pattern(pattern)) -> 
-                NotFound
-            | NotFound, expr ->
-                match expr |> traverseNode |> List.map (fun x -> x.AstNode) |> List.fold expressionReferencesIdentifier NotFound with
-                | Shadowed -> NotFound
-                | isFound -> isFound
-            | (isFound, _) -> isFound
-
-        match expression |> Expression |> expressionReferencesIdentifier NotFound with
-        | Found -> true
-        | NotFound | Shadowed -> false
-
     let validateLambdaCannotBeReplacedWithComposition lambda range visitorInfo =
         let canBeReplacedWithFunctionComposition expression = 
-            let removeLastElement = List.rev >> List.tail >> List.rev
-
             let getLastElement = List.rev >> List.head
 
-            let rec lambdaArgumentIsLastApplicationInFunctionCalls expression lambdaArgument numFunctionCalls =
-                let appliedValuesDoNotReferenceLambdaArgument appliedValues =
-                    let lambdaArgumentNotReferenced = expressionReferencesIdentifier lambdaArgument >> not
+            let rec lambdaArgumentIsLastApplicationInFunctionCalls expression (lambdaArgument:Ident) numFunctionCalls =
+                let rec appliedValuesAreConstants appliedValues =
+                    match appliedValues with
+                    | (SynExpr.Const(_)| SynExpr.Null(_))::rest -> appliedValuesAreConstants rest
+                    | [SynExpr.App(_) | SynExpr.Ident(_)] -> true
+                    | _ -> false
 
-                    List.isEmpty appliedValues |> not &&
-                    removeLastElement appliedValues
-                    |> List.forall lambdaArgumentNotReferenced
+                match AstNode.Expression expression with
+                | FuncApp((SynExpr.Ident(_) | SynExpr.LongIdent(_))::appliedValues, _)
+                        when appliedValuesAreConstants appliedValues -> 
 
-                match expression with
-                | SynExpr.App(_, false, _, _, _) as nonInfixApp ->
-                    match AstNode.Expression nonInfixApp with
-                    | FuncApp((SynExpr.Ident(_) | SynExpr.LongIdent(_))::appliedValues, _)
-                            when appliedValuesDoNotReferenceLambdaArgument appliedValues -> 
-
-                        match getLastElement appliedValues with
-                        | SynExpr.Ident(lastArgument) when numFunctionCalls > 1 -> 
-                            lastArgument.idText = lambdaArgument.idText
-                        | SynExpr.App(_, false, _, _, _) as nextFunction ->
-                            lambdaArgumentIsLastApplicationInFunctionCalls nextFunction lambdaArgument (numFunctionCalls + 1)
-                        | _ -> false
+                    match getLastElement appliedValues with
+                    | SynExpr.Ident(lastArgument) when numFunctionCalls > 1 -> 
+                        lastArgument.idText = lambdaArgument.idText
+                    | SynExpr.App(_, false, _, _, _) as nextFunction ->
+                        lambdaArgumentIsLastApplicationInFunctionCalls nextFunction lambdaArgument (numFunctionCalls + 1)
                     | _ -> false
                 | _ -> false
 
