4b74a4a64fbc67c42f9bcb9c2c24542e6b943386
Matt <matt@mattmcveigh.co.uk>
2016-06-19 10:53:13 +0100

    Updated authors

--- src/FSharpLint.Rules/XmlDocumentation.fs
+++ src/FSharpLint.Rules/XmlDocumentation.fs
@@ -1,5 +1,5 @@
 ﻿// FSharpLint, a linter for F#.
-// Copyright (C) 2016 Matthew Mcveigh
+// Copyright (C) 2016 Matthew Mcveigh, Jon Hamm
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
