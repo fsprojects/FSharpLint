ecad4188675f5a2851371f390925f2f2b10bd320
Matt <matt@mattmcveigh.co.uk>
2016-06-20 23:34:23 +0100

    Replace usage of system.type.assembly
    Property doesn't exist in dotnet core

--- src/FSharpLint.Framework/Resources.fs
+++ src/FSharpLint.Framework/Resources.fs
@@ -16,10 +16,13 @@
 
 namespace FSharpLint.Framework
 
+open System.Reflection
+open System.Resources
+
 /// Provides a way of getting string values from the framework's resource files (files in src/FSharpLint.Framework/Resources/).
 /// Used to retrieve multi-lingual strings inside of the app.
 type Resources() =
-    static let resourceManager = System.Resources.ResourceManager("Text", typeof<Resources>.Assembly)
+    static let resourceManager = ResourceManager("Text", typeof<Resources>.GetTypeInfo().Assembly)
 
     /// Returns the value of the specified string resource for the current culture.
     static member GetString(name) = resourceManager.GetString(name)
