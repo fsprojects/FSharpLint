5b0d12e46c919a4c856e8271deb42d803b0875e4
Matt <matt@mattmcveigh.co.uk>
2016-06-26 20:03:58 +0100

    Combined projects
    Rules, Framework, and Application all now inside core project - which is
    how it's to be represented as a nuget package (all together)

--- src/FSharpLint.Console/AssemblyInfo.fs
+++ src/FSharpLint.Console/AssemblyInfo.fs
@@ -4,9 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharpLint")>]
 [<assembly: AssemblyProductAttribute("FSharpLint")>]
 [<assembly: AssemblyDescriptionAttribute("Lint tool for F#.")>]
-[<assembly: AssemblyVersionAttribute("0.2.7")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.7")>]
+[<assembly: AssemblyVersionAttribute("0.3.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.7"
+    let [<Literal>] Version = "0.3.0"
+    let [<Literal>] InformationalVersion = "0.3.0"
