f8eec4450e960eab96e858d82a308dddebad99ba
Matt <matt@mattmcveigh.co.uk>
2016-06-30 22:56:59 +0100

    Added configuration manager back to api

--- src/FSharpLint.Core/Application/ConfigurationManager.fs
+++ src/FSharpLint.Core/Application/ConfigurationManager.fs
@@ -1,48 +1,34 @@
-﻿(*
-    FSharpLint, a linter for F#.
-    Copyright (C) 2014 Matthew Mcveigh
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*)
+﻿// FSharpLint, a linter for F#.
+// Copyright (C) 2014 Matthew Mcveigh
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace FSharpLint.Application
 
 module ConfigurationManager =
 
+    open System.Collections.Generic
     open System.IO
     open FSharpLint.Application.ConfigurationManagement
     open FSharpLint.Framework.Configuration
-
-    /// Check a config using checkers implemented by visitors.
-    /// These are used to verify hints are valid etc.
-    let private checkConfig config =
-        try
-            let configFailures = configCheckers() |> (checkConfigsForFailures config)
-
-            match configFailures with
-            | [] -> ConfigurationResult.Success(config)
-            | firstFailure::_ -> ConfigurationResult.Failure(FailedToLoadConfig(firstFailure)) 
-        with
-        | ConfigurationException(message) ->
-            ConfigurationResult.Failure(FailedToLoadConfig("Failed to load default config: " + message)) 
 
     /// Gets all the parent directories of a given path - includes the original path directory too.
     let private getParentDirectories path =
         let rec getParentDirectories parentDirectories (directoryInfo:DirectoryInfo) =
             match directoryInfo with
-                | null -> parentDirectories
-                | _ -> getParentDirectories (directoryInfo::parentDirectories) directoryInfo.Parent
+            | null -> parentDirectories
+            | _ -> getParentDirectories (directoryInfo::parentDirectories) directoryInfo.Parent
 
         DirectoryInfo path |> getParentDirectories []
 
@@ -53,15 +39,9 @@ module ConfigurationManager =
     /// If it fails to load the config any exception will be swallowed and `None` returned.
     /// If the file does not exist `None` will be returned.
     let private tryLoadConfig filePath =
-        if File.Exists(filePath) then
-            try
-                let newConfig = File.ReadAllText filePath |> configuration
-
-                match checkConfig newConfig with
-                    | ConfigurationResult.Success(config) -> Some(config)
-                    | ConfigurationResult.Failure(_) -> None
-            with
-                | _ -> None
+        if File.Exists filePath then
+            try File.ReadAllText filePath |> configuration |> Some
+            with _ -> None
         else
             None
 
@@ -70,11 +50,9 @@ module ConfigurationManager =
     /// Intended to allow for all the configuration files for all the projects in a solution
     /// to be grouped in a single place where they can be modified.
     type ConfigurationManager() =
-        let loadedConfigs = System.Collections.Generic.Dictionary<DirectoryInfo, Configuration>()
-                
-        let checkedDefaultConfiguration = checkConfig defaultConfiguration      
+        let loadedConfigs = Dictionary<DirectoryInfo, Configuration>()
 
-        member this.LoadConfigurationForProject(projectFilePath) =
+        member __.LoadConfigurationForProject(projectFilePath) =
             let getConfig (directory:DirectoryInfo) =
                 let filePath = Path.Combine(directory.FullName, SettingsFileName)
 
@@ -86,17 +64,17 @@ module ConfigurationManager =
                     | None -> None
                 
             Path.GetDirectoryName projectFilePath 
-                |> getParentDirectories
-                |> List.choose getConfig
-                |> List.iter loadedConfigs.Add
+            |> getParentDirectories
+            |> List.choose getConfig
+            |> List.iter loadedConfigs.Add
 
-        member this.GetConfigurationForProject(projectFilePath) =
+        member __.GetConfigurationForProject(projectFilePath) =
             let tryGetConfig dir =
-                match loadedConfigs.TryGetValue(dir) with
+                match loadedConfigs.TryGetValue dir with
                 | true, config -> Some(config)
                 | false, _ -> None
 
             Path.GetDirectoryName projectFilePath 
-                |> getParentDirectories
-                |> List.choose tryGetConfig
-                |> List.fold overrideConfiguration defaultConfiguration
+            |> getParentDirectories
+            |> List.choose tryGetConfig
+            |> List.fold overrideConfiguration defaultConfiguration
