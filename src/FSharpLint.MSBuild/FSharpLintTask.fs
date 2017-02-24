b127ae60e2581fc1eaf77addae170764115d9357
Matt <matt@mattmcveigh.co.uk>
2017-02-24 23:15:20 +0000

    Changed license from GPLv3 to MIT

--- src/FSharpLint.MSBuild/FSharpLintTask.fs
+++ src/FSharpLint.MSBuild/FSharpLintTask.fs
@@ -1,20 +1,4 @@
-﻿// FSharpLint, a linter for F#.
-// Copyright (C) 2016 Matthew Mcveigh
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-namespace FSharpLint.MSBuild
+﻿namespace FSharpLint.MSBuild
 
 open System
 open System.IO
