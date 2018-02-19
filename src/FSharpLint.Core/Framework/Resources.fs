b4b2ad58056a97c8fcbf4f254f02b2b85d208b68
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-02-19 19:08:58 +0000

    Address issue 252 -- be platform idependent by looking for the resource ending with the expected name, whether ot not it has a namespace prefix.

--- src/FSharpLint.Core/Framework/Resources.fs
+++ src/FSharpLint.Core/Framework/Resources.fs
@@ -6,7 +6,10 @@ open System.Resources
 /// Provides a way of getting string values from the framework's resource files (files in src/FSharpLint.Framework/Resources/).
 /// Used to retrieve multi-lingual strings inside of the app.
 type Resources() =
-    static let resourceManager = ResourceManager("Text", typeof<Resources>.GetTypeInfo().Assembly)
+    static let resourceName = Assembly.GetExecutingAssembly().GetManifestResourceNames()
+                              |> Seq.find (fun n -> n.EndsWith("Text.resources", System.StringComparison.Ordinal))
+
+    static let resourceManager = ResourceManager(resourceName.Replace(".resources", System.String.Empty), typeof<Resources>.GetTypeInfo().Assembly)
 
     /// Returns the value of the specified string resource for the current culture.
     static member GetString(name) = resourceManager.GetString(name)
