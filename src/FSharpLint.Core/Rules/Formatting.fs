e967af7a750858383742fb9eb64a5485d89e6fa1
Jason Gardella <jason.a.gardella@gmail.com>
2018-10-06 12:44:05 -0400

    Formatting tests.

--- src/FSharpLint.Core/Rules/Formatting.fs
+++ src/FSharpLint.Core/Rules/Formatting.fs
@@ -3,7 +3,6 @@
 module Formatting =
     
     open Microsoft.FSharp.Compiler.Ast
-    open Microsoft.FSharp.Compiler.SourceCodeServices
     open FSharpLint.Framework
     open FSharpLint.Framework.Analyser
     open FSharpLint.Framework.Ast
@@ -16,30 +15,27 @@ module Formatting =
         isRuleEnabled config AnalyserName ruleName |> Option.isSome
 
     /// Checks for correct spacing around colon of typed expression.
-    let private checkNamedTypeColonSpacing args synExpr isSuppressed =
-        let ruleName = "TypedExprSpacing"
+    let private checkNamedTypeColonSpacing args range isSuppressed =
+        let ruleName = "TypedItemSpacing"
 
         let isEnabled = isRuleEnabled args.Info.Config ruleName
 
         if isEnabled && isSuppressed ruleName |> not then
-            match synExpr with
-            | SynExpr.Typed (expr, typeName, range)  ->
-                args.Info.TryFindTextOfRange range
-                |> Option.iter (fun text ->
-                    match text.Split(':') with
-                    | [|exprText; typeText|] ->
-                        if exprText.TrimEnd(' ').Length <> exprText.Length - 1 
-                        || typeText.TrimStart(' ').Length <> typeText.Length - 1 then
-                            let suggestedFix = lazy(
-                                { FromRange = range; FromText = text; ToText = exprText + " : " + typeText }
-                                |> Some)
-                            args.Info.Suggest 
-                                { Range = range 
-                                  Message = "Use spaces around ':' in typed expression."
-                                  SuggestedFix = Some suggestedFix
-                                  TypeChecks = [] }
-                    | _ -> ())
-            | _ -> ()
+            args.Info.TryFindTextOfRange range
+            |> Option.iter (fun text ->
+                match text.Split(':') with
+                | [|otherText; typeText|] ->
+                    if otherText.TrimEnd(' ').Length <> otherText.Length - 1 
+                    || typeText.TrimStart(' ').Length <> typeText.Length - 1 then
+                        let suggestedFix = lazy(
+                            { FromRange = range; FromText = text; ToText = otherText + " : " + typeText }
+                            |> Some)
+                        args.Info.Suggest 
+                            { Range = range 
+                              Message = "Use spaces around ':' in typed expression."
+                              SuggestedFix = Some suggestedFix
+                              TypeChecks = [] }
+                | _ -> ())
 
     let analyser (args: AnalyserArgs) : unit = 
         let syntaxArray, skipArray = args.SyntaxArray, args.SkipArray
@@ -50,6 +46,6 @@ module Formatting =
             
         for i = 0 to syntaxArray.Length - 1 do
             match syntaxArray.[i].Actual with
-            | AstNode.Expression synExpr ->
-                checkNamedTypeColonSpacing args synExpr (isSuppressed i) 
+            | AstNode.Pattern (SynPat.Typed (_, _, range)) ->
+                checkNamedTypeColonSpacing args range (isSuppressed i) 
             | _ -> ()
---
+++ tests/FSharpLint.Core.Tests/Rules/TestFormattingRules.fs
@@ -0,0 +1,44 @@
+﻿module TestFormattingRules
+
+open NUnit.Framework
+open FSharpLint.Rules.Formatting
+open FSharpLint.Framework.Configuration
+
+let config = 
+    let ruleEnabled = { Rule.Settings = Map.ofList [ ("Enabled", Enabled(true)) ] }
+
+    Map.ofList 
+        [ (AnalyserName, 
+            { Rules = Map.ofList 
+                [ ("TypedItemSpacing", ruleEnabled) ]
+              Settings = Map.ofList [ ("Enabled", Enabled(true)) ] }) ]
+ 
+              
+[<TestFixture>]
+type Testformatting() =
+    inherit TestRuleBase.TestRuleBase(analyser, config)
+
+    [<Test>]
+    member this.``Error for typed pattern without spaces around colon``() = 
+        this.Parse("""
+module Program
+
+let (x:int) = 1""")
+
+        Assert.IsTrue(this.ErrorExistsAt(4, 5))
+
+    [<Test>]
+    member this.``Quickfix for typed pattern with missing spaces``() = 
+        let source = """
+module Program
+
+let (x:int) = 1"""
+
+
+        let expected = """
+module Program
+
+let (x : int) = 1"""
+
+        this.Parse source
+        Assert.AreEqual(expected, this.ApplyQuickFix source)
