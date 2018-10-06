5c2177a37791ad4f3469bce84aa6104c9a313952
Jason Gardella <jason.a.gardella@gmail.com>
2018-10-05 22:07:23 -0400

    Implement top-level namespace rule.

---
+++ src/FSharpLint.Core/Rules/Namespaces.fs
@@ -0,0 +1,54 @@
+﻿namespace FSharpLint.Rules
+
+module Namespaces =
+
+    open System
+    open System.IO
+    open FSharpLint.Framework
+    open FSharpLint.Framework.Analyser
+    open FSharpLint.Framework.Configuration
+    open Microsoft.FSharp.Compiler.Ast
+    open Microsoft.FSharp.Compiler.Range
+    open FSharpLint.Framework.Ast
+
+    [<Literal>]
+    let AnalyserName = "Namespaces"
+    
+    let private isAnalyserEnabled config =
+        isAnalyserEnabled config AnalyserName |> Option.isSome
+
+    let private isEnabled ruleName config =
+        isRuleEnabled config AnalyserName ruleName |> Option.isSome
+
+    module private TopLevelNamespace =
+
+        let checkTopLevelNamespace args range moduleOrNamespace isSuppressed =
+            let ruleName = "TopLevelNamespace"
+
+            let isEnabled = isEnabled ruleName args.Info.Config
+
+            if isEnabled && isSuppressed ruleName |> not then
+            let (SynModuleOrNamespace(_, _, isModule, _, _, _, _, _)) = moduleOrNamespace
+            if isModule
+            then
+                args.Info.Suggest
+                    { Range = range 
+                      Message = "Prefer namespaces at top level"
+                      SuggestedFix = None
+                      TypeChecks = [] }
+
+    let analyser (args: AnalyserArgs) : unit = 
+        if isAnalyserEnabled args.Info.Config then
+            let syntaxArray, skipArray = args.SyntaxArray, args.SkipArray
+
+            let isSuppressed i ruleName =
+                AbstractSyntaxArray.getSuppressMessageAttributes syntaxArray skipArray i 
+                |> AbstractSyntaxArray.isRuleSuppressed AnalyserName ruleName
+                
+            syntaxArray
+            |> Array.tryHead
+            |> Option.iter (fun firstNode ->
+                match firstNode.Actual with
+                | AstNode.ModuleOrNamespace moduleOrNamespace ->
+                    TopLevelNamespace.checkTopLevelNamespace args moduleOrNamespace.Range moduleOrNamespace (isSuppressed 0)
+                | _ -> ())
