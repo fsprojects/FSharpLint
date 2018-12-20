0d6adfe152c5174aeb80ca850ac8d2b7c4e1bca3
Matt Mcveigh <matt@mattmcveigh.co.uk>
2018-12-20 16:47:53 +0000

    Assume F# 4 is being used

--- tests/FSharpLint.FunctionalTest/TestConsoleApplication.fs
+++ tests/FSharpLint.FunctionalTest/TestConsoleApplication.fs
@@ -62,7 +62,7 @@ module Tests =
           "`not false` might be able to be refactored into `true`."
           "`List.fold ( + ) 0 x` might be able to be refactored into `List.sum x`."
           "`a <> true` might be able to be refactored into `not a`."
-          "`x = null`; suggestion: Consider using pattern matching, or if you're using F# 4 then `isNull`."
+          "`x = null` might be able to be refactored into `isNull x`."
           "`List.head (List.sort x)` might be able to be refactored into `List.min x`." ]
         
     [<TestFixture(Category = "Acceptance Tests")>]
