4adaf498155b7ca487c03395dd1e3ca98be9b9ac
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2019-05-29 23:47:35 +0200

    Update to FCS 29.0 (#342)
    * Update FCS to 29.0
    
    * Fix breaking changes in tests (FCS 29.0)

--- tests/FSharpLint.Core.Tests/Rules/TestNoTabCharactersRule.fs
+++ tests/FSharpLint.Core.Tests/Rules/TestNoTabCharactersRule.fs
@@ -1,6 +1,7 @@
 ﻿module TestNoTabCharactersRuleBase
 
 open FSharp.Compiler.SourceCodeServices
+open FSharp.Compiler.Text
 open FSharpLint.Application
 open FSharpLint.Application.ConfigurationManager
 open FSharpLint.Framework
@@ -9,28 +10,29 @@ open FSharpLint.Framework.Rules
 [<AbstractClass>]
 type TestNoTabCharactersRuleBase (rule:Rule) =
     inherit TestRuleBase.TestRuleBase()
-    
+
     override this.Parse (input:string, ?fileName:string, ?checkFile:bool) =
         let checker = FSharpChecker.Create()
-        
+        let sourceText = SourceText.ofString input
+
         let fileName = fileName |> Option.defaultValue "Test.fsx"
-        
-        let projectOptions, _ = checker.GetProjectOptionsFromScript(fileName, input) |> Async.RunSynchronously
+
+        let projectOptions, _ = checker.GetProjectOptionsFromScript(fileName, sourceText) |> Async.RunSynchronously
         let parsingOptions, _ = checker.GetParsingOptionsFromProjectOptions projectOptions
-        let parseResults = checker.ParseFile("test.fsx", input, parsingOptions) |> Async.RunSynchronously
-        
+        let parseResults = checker.ParseFile("test.fsx", sourceText, parsingOptions) |> Async.RunSynchronously
+
         let rule =
             match rule with
             | NoTabCharactersRule rule -> rule
             | _ -> failwithf "TestNoTabCharactersRuleBase only accepts NoTabCharactersRules"
-            
-        
+
+
         match parseResults.ParseTree with
-        | Some tree -> 
+        | Some tree ->
             let (syntaxArray, skipArray) = AbstractSyntaxArray.astToArray tree
             let (_, context) = runAstNodeRules Array.empty None input syntaxArray skipArray
             let lineRules = { LineRules.indentationRule = None; noTabCharactersRule = Some rule; genericLineRules = [||] }
-         
+
             runLineRules lineRules input context
             |> Array.iter this.postSuggestion
         | None -> ()
--- tests/FSharpLint.FunctionalTest/TestApi.fs
+++ tests/FSharpLint.FunctionalTest/TestApi.fs
@@ -7,37 +7,39 @@ module TestApi =
     open NUnit.Framework
     open FSharpLint.Application.Lint
     open FSharp.Compiler.SourceCodeServices
+    open FSharp.Compiler.Text
 
     let (</>) x y = Path.Combine(x, y)
 
     let basePath = TestContext.CurrentContext.TestDirectory </> ".." </> ".." </> ".." </> ".." </> ".."
 
     let sourceFile = basePath </> "tests" </> "TypeChecker.fs"
-    
+
     [<TestFixture(Category = "Acceptance Tests")>]
     type TestApi() =
         let generateAst source =
+            let sourceText = SourceText.ofString source
             let checker = FSharpChecker.Create()
 
-            let (options, _diagnostics) = 
-                checker.GetProjectOptionsFromScript(sourceFile, source) 
+            let (options, _diagnostics) =
+                checker.GetProjectOptionsFromScript(sourceFile, sourceText)
                 |> Async.RunSynchronously
 
             let parseResults =
-                checker.ParseFile(sourceFile, source, options |> checker.GetParsingOptionsFromProjectOptions |> fst)
+                checker.ParseFile(sourceFile, sourceText, options |> checker.GetParsingOptionsFromProjectOptions |> fst)
                 |> Async.RunSynchronously
-        
+
             match parseResults.ParseTree with
             | Some(parseTree) -> parseTree
             | None -> failwith "Failed to parse file."
 
         [<Category("Performance")>]
         [<Test>]
-        member __.``Performance of linting an existing file``() = 
+        member __.``Performance of linting an existing file``() =
             let text = File.ReadAllText sourceFile
             let tree = text |> generateAst
             let fileInfo = { Ast = tree; Source = text; TypeCheckResults = None }
-            
+
             let stopwatch = Stopwatch.StartNew()
             let times = ResizeArray()
 
@@ -45,7 +47,7 @@ module TestApi =
 
             for _ in 0..iterations do
                 stopwatch.Restart()
-                
+
                 let result = lintParsedFile OptionalLintParameters.Default fileInfo sourceFile
 
                 stopwatch.Stop()
@@ -53,7 +55,7 @@ module TestApi =
                 times.Add stopwatch.ElapsedMilliseconds
 
             let result = times |> Seq.sum |> (fun totalMilliseconds -> totalMilliseconds / int64 iterations)
-            
+
             Assert.Less(result, 250)
             System.Console.WriteLine(sprintf "Average runtime of linter on parsed file: %d (milliseconds)."  result)
 
