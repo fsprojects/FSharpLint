54e690708d041035c42a16592a5d1ae963636019
Volker Milbrandt <milbrandt@users.noreply.github.com>
2020-04-04 12:17:05 +0200

    Use fake and paket as dotnet tool (#401)
    * paket 5.243.0 (from  5.194.3) - supports netcoreapp3.1
    
    * .NET core SDK 3.1.200, Appveyor image and update fake
    
    - new AppVeyor Image Visual Studio 2019
    - update fake 5.19.1 (previous 5.18.3)
    - replace deprecated fake CliToolReference, see  https://github.com/fsharp/FAKE/issues/2465
    - use .NET core SDK 3.1.200
    
    * use paket as dotnet tool
    
    see also [Announcement: The `dotnet-fake` package and CliToolReference (Paket `clitool`) is deprecated](https://github.com/fsharp/FAKE/issues/2465=
    
    * remve dependencyfrom fake-cli in paket.paket.dependencies (fake is used as dotnet tool now)

--- build.cmd
+++ build.cmd
@@ -1,6 +1,9 @@
 @echo off
 cls
 
+# Restore the tool NuGet packages
+dotnet tool restore
+
 dotnet restore build.proj --verbosity n
 
 dotnet fake run build.fsx
--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 set -eu
 set -o pipefail
 
+# Restore the tool NuGet packages
+dotnet tool restore
+
 dotnet restore build.proj --verbosity n
 
 dotnet fake run build.fsx
