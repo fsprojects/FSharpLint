dca944f475906ac3898b52a60eae63f1590c77cd
Jason Gardella <jason.a.gardella@gmail.com>
2020-05-13 15:03:00 -0400

    Update

--- tests/FSharpLint.FunctionalTest/TestApi.fs
+++ tests/FSharpLint.FunctionalTest/TestApi.fs
@@ -3,8 +3,11 @@
 module TestApi =
 
     open System.IO
+    open System.Diagnostics
     open NUnit.Framework
     open FSharpLint.Application.Lint
+    open FSharp.Compiler.SourceCodeServices
+    open FSharp.Compiler.Text
 
     let (</>) x y = Path.Combine(x, y)
 
@@ -14,6 +17,47 @@ module TestApi =
 
     [<TestFixture(Category = "Acceptance Tests")>]
     type TestApi() =
+        let generateAst source =
+            let sourceText = SourceText.ofString source
+            let checker = FSharpChecker.Create()
+
+            let (options, _diagnostics) =
+                checker.GetProjectOptionsFromScript(sourceFile, sourceText)
+                |> Async.RunSynchronously
+
+            let parseResults =
+                checker.ParseFile(sourceFile, sourceText, options |> checker.GetParsingOptionsFromProjectOptions |> fst)
+                |> Async.RunSynchronously
+
+            match parseResults.ParseTree with
+            | Some(parseTree) -> parseTree
+            | None -> failwith "Failed to parse file."
+
+        [<Category("Performance")>]
+        [<Test>]
+        member __.``Performance of linting an existing file``() =
+            let text = File.ReadAllText sourceFile
+            let tree = text |> generateAst
+            let fileInfo = { Ast = tree; Source = text; TypeCheckResults = None }
+
+            let stopwatch = Stopwatch.StartNew()
+            let times = ResizeArray()
+
+            let iterations = 100
+
+            for _ in 0..iterations do
+                stopwatch.Restart()
+
+                lintParsedFile OptionalLintParameters.Default fileInfo sourceFile |> ignore
+
+                stopwatch.Stop()
+
+                times.Add stopwatch.ElapsedMilliseconds
+
+            let result = times |> Seq.sum |> (fun totalMilliseconds -> totalMilliseconds / int64 iterations)
+
+            Assert.Less(result, 250)
+            System.Console.WriteLine(sprintf "Average runtime of linter on parsed file: %d (milliseconds)."  result)
 
         [<Test>]
         member __.``Lint project via absolute path``() =
