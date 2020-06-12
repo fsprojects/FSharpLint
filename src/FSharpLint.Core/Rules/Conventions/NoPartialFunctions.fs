8603fe1f28baeeffcaca1d6c485b71f9bb01101e
Jason Gardella <jason.a.gardella@gmail.com>
2020-06-12 17:22:58 -0400

    Add collection functions

--- src/FSharpLint.Core/Rules/Conventions/NoPartialFunctions.fs
+++ src/FSharpLint.Core/Rules/Conventions/NoPartialFunctions.fs
@@ -15,8 +15,45 @@ let private partialFunctions =
         // Option
         ("Option.get", PatternMatch)
 
+        // Array
+        ("Array.exactlyOne", Function "Array.tryExactlyOne")
+        ("Array.get", Function "Array.tryItem")
+        ("Array.item", Function "Array.tryItem")
+        ("Array.find", Function "Array.tryFind")
+        ("Array.findIndex", Function "Array.tryFindIndex")
+        ("Array.findBack", Function "Array.tryFindBack")
+        ("Array.head", Function "Array.tryHead")
+        ("Array.last", Function "Array.tryLast")
+        ("Array.tail", Function "Array.tryLast")
+        ("Array.reduce", Function "Array.fold")
+        ("Array.reduceBack", Function "Array.foldBack")
+        ("Array.pick", Function "Array.tryPick")
+
+        // Seq
+        ("Seq.exactlyOne", Function "Seq.tryExactlyOne")
+        ("Seq.item", Function "Seq.tryItem")
+        ("Seq.find", Function "Seq.tryFind")
+        ("Seq.findIndex", Function "Seq.tryFindIndex")
+        ("Seq.findBack", Function "Seq.tryFindBack")
+        ("Seq.head", Function "Seq.tryHead")
+        ("Seq.last", Function "Seq.tryLast")
+        ("Seq.tail", Function "Seq.tryLast")
+        ("Seq.reduce", Function "Seq.fold")
+        ("Seq.reduceBack", Function "Seq.foldBack")
+        ("Seq.pick", Function "Seq.tryPick")
+
         // List
+        ("List.exactlyOne", Function "List.tryExactlyOne")
+        ("List.item", Function "List.tryItem")
         ("List.find", Function "List.tryFind")
+        ("List.findIndex", Function "List.tryFindIndex")
+        ("List.findBack", Function "List.tryFindBack")
+        ("List.head", Function "List.tryHead")
+        ("List.last", Function "List.tryLast")
+        ("List.tail", Function "List.tryLast")
+        ("List.reduce", Function "List.fold")
+        ("List.reduceBack", Function "List.foldBack")
+        ("List.pick", Function "List.tryPick")
     ] |> Map.ofList
 
 let private checkIfPartial (identifier:string) (range:range) =
