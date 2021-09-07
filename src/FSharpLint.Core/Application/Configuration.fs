fsprojects/FSharpLint

commit 8140983c6a865103f601ce72379769e6491a0adb

Separate non public values rule in two rules (#495)
* Separate nonPublicValuesNames rule in two rules

Fixes https://github.com/fsprojects/FSharpLint/issues/493

Co-authored-by: Andres G. Aragoneses <knocte@gmail.com>

* Backwards compatibility for nonPublicValues

(But we still throw only if mixing old setting with any of the
new settings.)

Co-authored-by: Retheesh <sumod8898@gmail.com>

src/FSharpLint.Core/Application/Configuration.fs
@@ -244,7 +244,9 @@ type NamesConfig =
       measureTypeNames:RuleConfig<NamingConfig> option
       activePatternNames:RuleConfig<NamingConfig> option
       publicValuesNames:RuleConfig<NamingConfig> option
-      nonPublicValuesNames:RuleConfig<NamingConfig> option }
+      nonPublicValuesNames:RuleConfig<NamingConfig> option
+      privateValuesNames:RuleConfig<NamingConfig> option
+      internalValuesNames:RuleConfig<NamingConfig> option }
 with
     member this.Flatten() =
         [|
@@ -262,7 +264,10 @@ with
             this.measureTypeNames |> Option.bind (constructRuleWithConfig MeasureTypeNames.rule) |> Option.toArray
             this.activePatternNames |> Option.bind (constructRuleWithConfig ActivePatternNames.rule) |> Option.toArray
             this.publicValuesNames |> Option.bind (constructRuleWithConfig PublicValuesNames.rule) |> Option.toArray
-            this.nonPublicValuesNames |> Option.bind (constructRuleWithConfig NonPublicValuesNames.rule) |> Option.toArray
+            this.nonPublicValuesNames |> Option.bind (constructRuleWithConfig PrivateValuesNames.rule) |> Option.toArray
+            this.nonPublicValuesNames |> Option.bind (constructRuleWithConfig InternalValuesNames.rule) |> Option.toArray
+            this.privateValuesNames |> Option.bind (constructRuleWithConfig PrivateValuesNames.rule) |> Option.toArray
+            this.internalValuesNames|> Option.bind (constructRuleWithConfig InternalValuesNames.rule) |> Option.toArray
         |] |> Array.concat
 
 type NumberOfItemsConfig =
@@ -409,6 +414,8 @@ type Configuration =
       ActivePatternNames:RuleConfig<NamingConfig> option
       PublicValuesNames:RuleConfig<NamingConfig> option
       NonPublicValuesNames:RuleConfig<NamingConfig> option
+      PrivateValuesNames:RuleConfig<NamingConfig> option
+      InternalValuesNames:RuleConfig<NamingConfig> option
       MaxNumberOfItemsInTuple:RuleConfig<Helper.NumberOfItems.Config> option
       MaxNumberOfFunctionParameters:RuleConfig<Helper.NumberOfItems.Config> option
       MaxNumberOfMembers:RuleConfig<Helper.NumberOfItems.Config> option
@@ -483,6 +490,8 @@ with
         ActivePatternNames = None
         PublicValuesNames = None
         NonPublicValuesNames = None
+        PrivateValuesNames = None
+        InternalValuesNames = None
         MaxNumberOfItemsInTuple = None
         MaxNumberOfFunctionParameters = None
         MaxNumberOfMembers = None
@@ -619,7 +628,10 @@ let flattenConfig (config:Configuration) =
             config.MeasureTypeNames |> Option.bind (constructRuleWithConfig MeasureTypeNames.rule)
             config.ActivePatternNames |> Option.bind (constructRuleWithConfig ActivePatternNames.rule)
             config.PublicValuesNames |> Option.bind (constructRuleWithConfig PublicValuesNames.rule)
-            config.NonPublicValuesNames |> Option.bind (constructRuleWithConfig NonPublicValuesNames.rule)
+            config.NonPublicValuesNames |> Option.bind (constructRuleWithConfig PrivateValuesNames.rule)
+            config.NonPublicValuesNames |> Option.bind (constructRuleWithConfig InternalValuesNames.rule)
+            config.PrivateValuesNames |> Option.bind (constructRuleWithConfig PrivateValuesNames.rule)
+            config.InternalValuesNames |> Option.bind (constructRuleWithConfig InternalValuesNames.rule)
             config.MaxNumberOfItemsInTuple |> Option.bind (constructRuleWithConfig MaxNumberOfItemsInTuple.rule)
             config.MaxNumberOfFunctionParameters |> Option.bind (constructRuleWithConfig MaxNumberOfFunctionParameters.rule)
             config.MaxNumberOfMembers |> Option.bind (constructRuleWithConfig MaxNumberOfMembers.rule)
@@ -636,6 +648,10 @@ let flattenConfig (config:Configuration) =
             config.NoTabCharacters |> Option.bind (constructRuleIfEnabled NoTabCharacters.rule)
             config.NoPartialFunctions |> Option.bind (constructRuleWithConfig NoPartialFunctions.rule)
         |] |> Array.choose id
+
+    if config.NonPublicValuesNames.IsSome &&
+        (config.PrivateValuesNames.IsSome || config.PublicValuesNames.IsSome) then
+        failwith "nonPublicValuesNames has been deprecated, use privateValuesNames and/or internalValuesNames instead"
 
     let astNodeRules = ResizeArray()
     let lineRules = ResizeArray()

src/FSharpLint.Core/FSharpLint.Core.fsproj
@@ -84,7 +84,8 @@
     <Compile Include="Rules\Conventions\Naming\MeasureTypeNames.fs" />
     <Compile Include="Rules\Conventions\Naming\ActivePatternNames.fs" />
     <Compile Include="Rules\Conventions\Naming\PublicValuesNames.fs" />
-    <Compile Include="Rules\Conventions\Naming\NonPublicValuesNames.fs" />
+    <Compile Include="Rules\Conventions\Naming\PrivateValuesNames.fs" />
+    <Compile Include="Rules\Conventions\Naming\InternalValuesNames.fs" />
     <Compile Include="Rules\Conventions\NumberOfItems\NumberOfItemsHelper.fs" />
     <Compile Include="Rules\Conventions\NumberOfItems\MaxNumberOfItemsInTuple.fs" />
     <Compile Include="Rules\Conventions\NumberOfItems\MaxNumberOfFunctionParameters.fs" />

src/FSharpLint.Core/Rules/Conventions/Naming/ActivePatternNames.fs
@@ -25,13 +25,13 @@ let private getValueOrFunctionIdents _ pattern =
 let private getIdentifiers (args:AstNodeRuleParams) =
     match args.AstNode with
     | AstNode.Expression(SynExpr.ForEach(_, _, true, pattern, _, _, _)) ->
-        getPatternIdents false getValueOrFunctionIdents false pattern
+        getPatternIdents Accessibility.Private getValueOrFunctionIdents false pattern
     | AstNode.Binding(SynBinding(_, _, _, _, attributes, _, valData, pattern, _, _, _, _)) ->
         if not (isLiteral attributes) then
             match identifierTypeFromValData valData with
             | Value | Function ->
-                let isPublic = isPublic args.SyntaxArray args.NodeIndex
-                getPatternIdents isPublic getValueOrFunctionIdents true pattern
+                let accessibility = getAccessibility args.SyntaxArray args.NodeIndex
+                getPatternIdents accessibility getValueOrFunctionIdents true pattern
             | _ -> Array.empty
         else
             Array.empty

src/FSharpLint.Core/Rules/Conventions/Naming/InternalValuesNames.fs
@@ -1,4 +1,4 @@
-module FSharpLint.Rules.PublicValuesNames
+module FSharpLint.Rules.InternalValuesNames
 
 open FSharp.Compiler.Syntax
 open FSharpLint.Framework.Ast
@@ -7,14 +7,10 @@ open FSharpLint.Framework.Rules
 open FSharpLint.Rules.Helper.Naming
 
 let private getValueOrFunctionIdents typeChecker isPublic pattern =
-    let checkNotUnionCase ident = fun () -> 
+    let checkNotUnionCase ident = fun () ->
         typeChecker
         |> Option.map (fun checker -> isNotUnionCase checker ident)
         |> Option.defaultValue false
-
-    let isNotActivePattern (ident:Ident) =
-        ident.idText.StartsWith("|")
-        |> not
 
     match pattern with
     | SynPat.LongIdent(longIdent, _, _, _, _, _) ->
@@ -22,9 +18,9 @@ let private getValueOrFunctionIdents typeChecker isPublic pattern =
         let singleIdentifier = List.length longIdent.Lid = 1
 
         match List.tryLast longIdent.Lid with
-        | Some ident when singleIdentifier ->
+        | Some ident when not (isActivePattern ident) && singleIdentifier ->
             let checkNotUnionCase = checkNotUnionCase ident
-            if isPublic = Accessibility.Public && isNotActivePattern ident then
+            if isPublic = Accessibility.Internal then
                 (ident, ident.idText, Some checkNotUnionCase)
                 |> Array.singleton
             else
@@ -34,10 +30,8 @@ let private getValueOrFunctionIdents typeChecker isPublic pattern =
 
 let private getIdentifiers (args:AstNodeRuleParams) =
     match args.AstNode with
-    | AstNode.Expression(SynExpr.ForEach(_, _, true, pattern, _, _, _)) ->
-        getPatternIdents Accessibility.Private (getValueOrFunctionIdents args.CheckInfo) false pattern
-    | AstNode.Binding(SynBinding(_, _, _, _, attributes, _, valData, pattern, _, _, _, _)) ->
-        if not (isLiteral attributes || isExtern attributes) then
+    | AstNode.Binding(SynBinding(access, _, _, _, attributes, _, valData, pattern, _, _, _, _)) ->
+        if not (isLiteral attributes) then
             match identifierTypeFromValData valData with
             | Value | Function ->
                 let accessibility = getAccessibility args.SyntaxArray args.NodeIndex
@@ -48,8 +42,8 @@ let private getIdentifiers (args:AstNodeRuleParams) =
     | _ -> Array.empty
 
 let rule config =
-    { Name = "PublicValuesNames"
-      Identifier = Identifiers.PublicValuesNames
+    { Name = "InternalValuesNames"
+      Identifier = Identifiers.InternalValuesNames
       RuleConfig = { NamingRuleConfig.Config = config; GetIdentifiersToCheck = getIdentifiers } }
     |> toAstNodeRule
     |> AstNodeRule

src/FSharpLint.Core/Rules/Conventions/Naming/MemberNames.fs
@@ -29,7 +29,7 @@ let private getIdentifiers (args:AstNodeRuleParams) =
         if not (isLiteral attributes) && not (isImplementingInterface parents) then
             match identifierTypeFromValData valData with
             | Member | Property ->
-                getPatternIdents false getMemberIdents true pattern
+                getPatternIdents Accessibility.Private getMemberIdents true pattern
             | _ -> Array.empty
         else
             Array.empty

src/FSharpLint.Core/Rules/Conventions/Naming/NamingHelper.fs
@@ -178,14 +178,21 @@ let activePatternIdentifiers (identifier:Ident) =
     |> Seq.toArray
     |> Array.filter (fun x -> not <| String.IsNullOrEmpty(x) && x.Trim() <> "_")
 
-let isPublic (syntaxArray:AbstractSyntaxArray.Node []) i =
-    let isSynAccessPublic = function
-        | Some(SynAccess.Public) | None -> true
-        | _ -> false
 
-    let rec isPublic publicSoFar isPrivateWhenReachedBinding i =
-        if i = 0 then publicSoFar
-        else if publicSoFar then
+type Accessibility = 
+    | Public
+    | Private
+    | Internal
+
+let getAccessibility (syntaxArray:AbstractSyntaxArray.Node []) i =
+    let isSynAccessPublic = function
+        | Some(SynAccess.Public) | None -> Accessibility.Public
+        | Some(SynAccess.Private) -> Accessibility.Private
+        | Some(SynAccess.Internal) -> Accessibility.Internal
+
+    let rec getAccessibility state isPrivateWhenReachedBinding i =
+        if i = 0 then state
+        else
             let node = syntaxArray.[i]
             match node.Actual with
             | TypeSimpleRepresentation(SynTypeDefnSimpleRepr.Record(access, _, _))
@@ -197,15 +204,15 @@ let isPublic (syntaxArray:AbstractSyntaxArray.Node []) i =
             | ExceptionRepresentation(SynExceptionDefnRepr.SynExceptionDefnRepr(_, _, _, _, access, _))
             | Pattern(SynPat.Named(_, _, _, access, _))
             | Pattern(SynPat.LongIdent(_, _, _, _, access, _)) ->
-                isPublic (isSynAccessPublic access) isPrivateWhenReachedBinding node.ParentIndex
+                getAccessibility (isSynAccessPublic access) isPrivateWhenReachedBinding node.ParentIndex
             | TypeSimpleRepresentation(_)
-            | Pattern(_) -> true
+            | Pattern(_) -> Accessibility.Public
             | MemberDefinition(_) ->
-                if isPrivateWhenReachedBinding then false
-                else isPublic publicSoFar isPrivateWhenReachedBinding node.ParentIndex
+                if isPrivateWhenReachedBinding then Accessibility.Private
+                else getAccessibility state isPrivateWhenReachedBinding node.ParentIndex
             | Binding(SynBinding(access, _, _, _, _, _, _, _, _, _, _, _)) ->
-                if isPrivateWhenReachedBinding then false
-                else isPublic (isSynAccessPublic access) true node.ParentIndex
+                if isPrivateWhenReachedBinding then Accessibility.Private
+                else getAccessibility (isSynAccessPublic access) true node.ParentIndex
             | EnumCase(_)
             | TypeRepresentation(_)
             | Type(_)
@@ -218,14 +225,14 @@ let isPublic (syntaxArray:AbstractSyntaxArray.Node []) i =
             | SimplePattern(_)
             | File(_)
             | LambdaArg(_)
-            | SimplePatterns(_) -> isPublic publicSoFar isPrivateWhenReachedBinding node.ParentIndex
+            | SimplePatterns(_) -> getAccessibility state isPrivateWhenReachedBinding node.ParentIndex
             | TypeDefinition(_)
             | Else(_)
             | LambdaBody(_)
-            | Expression(_) -> isPublic publicSoFar true node.ParentIndex
-        else false
+            | Expression(_) -> getAccessibility state true node.ParentIndex
 
-    isPublic true false i
+    getAccessibility Accessibility.Public false i
+
 
 /// Is an attribute with a given name?
 /// e.g. check for Literal attribute.
@@ -273,9 +280,10 @@ let isInterface typeDef =
         members |> List.forall canBeInInterface
     | _ -> false
 
-let checkIfPublic isCurrentlyPublic = function
-    | Some(SynAccess.Public) | None -> isCurrentlyPublic
-    | Some(SynAccess.Internal | SynAccess.Private) -> false
+let checkAccessibility currentAccessibility = function
+    | Some(SynAccess.Public) | None -> currentAccessibility
+    | Some(SynAccess.Private) -> Accessibility.Private
+    | Some(SynAccess.Internal) -> Accessibility.Internal
 
 let isModule (moduleKind:SynModuleOrNamespaceKind) =
     match moduleKind with
@@ -294,12 +302,12 @@ let isImplicitModule (SynModuleOrNamespace.SynModuleOrNamespace(longIdent, _, mo
     // TODO: does SynModuleOrNamespaceKind.AnonModule replace this check?
     isModule moduleKind && longIdent |> List.forall (fun x -> zeroLengthRange x.idRange)
 
-type GetIdents<'t> = bool -> SynPat -> 't []
+type GetIdents<'t> = Accessibility -> SynPat -> 't []
 
-let rec getPatternIdents<'t> isPublic (getIdents:GetIdents<'t>) argsAreParameters pattern =
+let rec getPatternIdents<'t> accessibility (getIdents:GetIdents<'t>) argsAreParameters pattern =
     match pattern with
     | SynPat.LongIdent(_, _, _, args, access, _) ->
-        let isPublic = checkIfPublic isPublic access
+        let isPublic = checkAccessibility accessibility access
 
         let hasNoArgs =
             match args with
@@ -311,11 +319,11 @@ let rec getPatternIdents<'t> isPublic (getIdents:GetIdents<'t>) argsAreParameter
             | SynArgPats.NamePatPairs(pats, _) ->
                 pats
                 |> List.toArray
-                |> Array.collect (snd >> getPatternIdents false getIdents false)
+                |> Array.collect (snd >> getPatternIdents Accessibility.Private getIdents false)
             | SynArgPats.Pats(pats) ->
                 pats
                 |> List.toArray
-                |> Array.collect (getPatternIdents false getIdents false)
+                |> Array.collect (getPatternIdents Accessibility.Private getIdents false)
 
         // Only check if expecting args as parameters e.g. function - otherwise is a DU pattern.
         if hasNoArgs || argsAreParameters then
@@ -324,20 +332,20 @@ let rec getPatternIdents<'t> isPublic (getIdents:GetIdents<'t>) argsAreParameter
         else
             argSuggestions
     | SynPat.Named(p, _, _, access, _) ->
-        let isPublic = checkIfPublic isPublic access
-        getIdents isPublic pattern
-        |> Array.append (getPatternIdents isPublic getIdents false p)
+        let accessibility = checkAccessibility accessibility access
+        getIdents accessibility pattern
+        |> Array.append (getPatternIdents accessibility getIdents false p)
     | SynPat.Or(p1, p2, _) ->
         [|p1; p2|]
-        |> Array.collect (getPatternIdents isPublic getIdents false)
+        |> Array.collect (getPatternIdents accessibility getIdents false)
     | SynPat.Paren(p, _) ->
-        getPatternIdents isPublic getIdents false p
+        getPatternIdents accessibility getIdents false p
     | SynPat.Ands(pats, _)
     | SynPat.Tuple(_, pats, _)
     | SynPat.ArrayOrList(_, pats, _) ->
         pats
         |> List.toArray
-        |> Array.collect (getPatternIdents isPublic getIdents false)
+        |> Array.collect (getPatternIdents accessibility getIdents false)
     | SynPat.Record(_)
     | SynPat.IsInst(_)
     | SynPat.QuoteExpr(_)

src/FSharpLint.Core/Rules/Conventions/Naming/ParameterNames.fs
@@ -12,7 +12,7 @@ let private getMemberIdents _ = function
         (ident, ident.idText, None) |> Array.singleton
     | _ -> Array.empty
 
-let private getValueOrFunctionIdents typeChecker isPublic pattern =
+let private getValueOrFunctionIdents typeChecker _accessibility pattern =
     let checkNotUnionCase ident = fun () ->
         typeChecker
         |> Option.map (fun checker -> isNotUnionCase checker ident)
@@ -40,15 +40,15 @@ let private getIdentifiers (args:AstNodeRuleParams) =
         if not (isLiteral attributes) then
             match identifierTypeFromValData valData with
             | Value | Function ->
-                let isPublic = isPublic args.SyntaxArray args.NodeIndex
-                getPatternIdents isPublic (getValueOrFunctionIdents args.CheckInfo) true pattern
+                let accessibility = getAccessibility args.SyntaxArray args.NodeIndex
+                getPatternIdents accessibility (getValueOrFunctionIdents args.CheckInfo) true pattern
             | Member | Property ->
-                getPatternIdents false getMemberIdents true pattern
+                getPatternIdents Accessibility.Private getMemberIdents true pattern
             | _ -> Array.empty
         else
             Array.empty
     | AstNode.Expression(SynExpr.ForEach(_, _, true, pattern, _, _, _)) ->
-        getPatternIdents false (getValueOrFunctionIdents args.CheckInfo) false pattern
+        getPatternIdents Accessibility.Private (getValueOrFunctionIdents args.CheckInfo) false pattern
     | _ -> Array.empty
 
 let rule config =

src/FSharpLint.Core/Rules/Conventions/Naming/PrivateValuesNames.fs
@@ -1,4 +1,4 @@
-module FSharpLint.Rules.NonPublicValuesNames
+module FSharpLint.Rules.PrivateValuesNames
 
 open FSharp.Compiler.Syntax
 open FSharpLint.Framework.Ast
@@ -6,7 +6,7 @@ open FSharpLint.Framework.AstInfo
 open FSharpLint.Framework.Rules
 open FSharpLint.Rules.Helper.Naming
 
-let private getValueOrFunctionIdents typeChecker isPublic pattern =
+let private getValueOrFunctionIdents typeChecker accessibility pattern =
     let checkNotUnionCase ident = fun () -> 
         typeChecker
         |> Option.map (fun checker -> isNotUnionCase checker ident)
@@ -20,7 +20,7 @@ let private getValueOrFunctionIdents typeChecker isPublic pattern =
         match List.tryLast longIdent.Lid with
         | Some ident when not (isActivePattern ident) && singleIdentifier ->
             let checkNotUnionCase = checkNotUnionCase ident
-            if not isPublic then
+            if accessibility = Accessibility.Private then
                 (ident, ident.idText, Some checkNotUnionCase)
                 |> Array.singleton
             else
@@ -31,13 +31,13 @@ let private getValueOrFunctionIdents typeChecker isPublic pattern =
 let private getIdentifiers (args:AstNodeRuleParams) =
     match args.AstNode with
     | AstNode.Expression(SynExpr.ForEach(_, _, true, pattern, _, _, _)) ->
-        getPatternIdents false (getValueOrFunctionIdents args.CheckInfo) false pattern
+        getPatternIdents Accessibility.Private (getValueOrFunctionIdents args.CheckInfo) false pattern
     | AstNode.Binding(SynBinding(access, _, _, _, attributes, _, valData, pattern, _, _, _, _)) ->
         if not (isLiteral attributes) then
             match identifierTypeFromValData valData with
             | Value | Function ->
-                let isPublic = isPublic args.SyntaxArray args.NodeIndex
-                getPatternIdents isPublic (getValueOrFunctionIdents args.CheckInfo) true pattern
+                let accessibility = getAccessibility args.SyntaxArray args.NodeIndex
+                getPatternIdents accessibility (getValueOrFunctionIdents args.CheckInfo) true pattern
             | _ -> Array.empty
         else
             Array.empty
@@ -51,8 +51,8 @@ let private getIdentifiers (args:AstNodeRuleParams) =
     | _ -> Array.empty
 
 let rule config =
-    { Name = "NonPublicValuesNames"
-      Identifier = Identifiers.NonPublicValuesNames
+    { Name = "PrivateValuesNames"
+      Identifier = Identifiers.PrivateValuesNames
       RuleConfig = { NamingRuleConfig.Config = config; GetIdentifiersToCheck = getIdentifiers } }
     |> toAstNodeRule
     |> AstNodeRule

commit 9b6621feda661ed4d89068507ab02a6374d4699d

Hotfix: fix nonPublicValues backward compatiblity (#498)
* Hotfix: fix nonPublicValues backward compat

This typo was introduced in 8140983c6a865103f601ce72379769e6491a0adb.

* Bump version

src/FSharpLint.Core/Application/Configuration.fs
@@ -650,7 +650,7 @@ let flattenConfig (config:Configuration) =
         |] |> Array.choose id
 
     if config.NonPublicValuesNames.IsSome &&
-        (config.PrivateValuesNames.IsSome || config.PublicValuesNames.IsSome) then
+        (config.PrivateValuesNames.IsSome || config.InternalValuesNames.IsSome) then
         failwith "nonPublicValuesNames has been deprecated, use privateValuesNames and/or internalValuesNames instead"
 
     let astNodeRules = ResizeArray()