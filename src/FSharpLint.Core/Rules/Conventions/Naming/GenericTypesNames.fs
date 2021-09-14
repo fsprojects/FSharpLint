8e5cda2c071b920a73ef2ff03880bc777535a31f
Retheesh <sumod8898@gmail.com>
2021-09-14 14:54:15 +0200

    Add new rule GenericTypesNames
    More info: https://twitter.com/dsymetweets/status/1235330491311677440

---
+++ src/FSharpLint.Core/Rules/Conventions/Naming/GenericTypesNames.fs
@@ -0,0 +1,30 @@
+﻿module FSharpLint.Rules.GenericTypesNames
+
+open FSharp.Compiler.Syntax
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.AstInfo
+open FSharpLint.Framework.Rules
+open FSharpLint.Rules.Helper.Naming
+
+let private getIdentifiers (args: AstNodeRuleParams) =
+    match args.AstNode with
+    | AstNode.TypeDefinition(SynTypeDefn(componentInfo, typeDef, _, _, _)) ->
+        match componentInfo with
+        | SynComponentInfo(attrs, types, _, identifier, _, _, _, _) ->
+            match List.tryLast types with
+            | Some synType ->
+                match synType with
+                | SynTyparDecl(_attr, synTypeDecl) ->  
+                    match synTypeDecl with
+                    | SynTypar(id, _, _) when not (isPascalCase id.idText) ->
+                        (id, id.idText, None) |> Array.singleton
+                    | _ -> Array.empty
+            | _ -> Array.empty       
+    | _ -> Array.empty
+
+let rule config =
+    { Name = "GenericTypesNames"
+      Identifier = Identifiers.GenericTypesNames
+      RuleConfig = { NamingRuleConfig.Config = config; GetIdentifiersToCheck = getIdentifiers } }
+    |> toAstNodeRule
+    |> AstNodeRule
---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/GenericTypesNames.fs
@@ -0,0 +1,30 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.GenericTypesNames
+
+open NUnit.Framework
+open FSharpLint.Framework.Rules
+open FSharpLint.Rules
+
+let config =
+    { NamingConfig.Naming = Some NamingCase.PascalCase
+      Underscores = Some NamingUnderscores.None
+      Prefix = None
+      Suffix = None }
+[<TestFixture>]
+type TestConventionsGenericTypesNames() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(GenericTypesNames.rule config)
+
+    [<Test>]
+    member this.GenericTypeNameIsPascalCase() =
+        this.Parse """
+type Foo<'T> = Option<'T>
+"""
+        this.AssertNoWarnings()
+        
+    [<Test>]
+    member this.``generic type name shouldn't be camelCase``() =
+        this.Parse """
+type Foo<'a> = Option<'a>
+    """
+        Assert.IsTrue(this.ErrorsExist)
+        Assert.IsTrue(this.ErrorExistsAt(2, 9))
+
