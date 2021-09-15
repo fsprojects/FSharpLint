77bb9ab9777e32cc8871dd5e4719721a62fd4de2
Retheesh <sumod8898@gmail.com>
2021-09-15 06:03:01 +0200

    Improve GenericTypesNames to cover even more cases

--- tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/GenericTypesNames.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/GenericTypesNames.fs
@@ -29,9 +29,31 @@ type Foo<'a> = Option<'a>
         Assert.IsTrue(this.ErrorExistsAt(2, 9))
 
     [<Test>]
-    member this.``generic type name shouldn't be camelCase (2 generic types)``() =
+    member this.``generic type names shouldn't be camelCase (2 generic types)``() =
         this.Parse """
 type Foo<'a, 'T> = Option<'a * 'T>
 """
         Assert.IsTrue(this.ErrorsExist)
         Assert.IsTrue(this.ErrorExistsAt(2, 9))
+
+    [<Test>]
+    member this.``generic type names shouldn't be camelCase (2 generic types with different order)``() =
+        this.Parse """
+type Foo<'T, 'a> = Option<'T * 'a>
+"""
+        Assert.IsTrue(this.ErrorsExist)
+        Assert.IsTrue(this.ErrorExistsAt(2, 13))
+
+    [<Test>]
+    member this.``generic type names are PascalCase``() =
+        this.Parse """
+type Foo<'K, 'V> = Option<'K * 'V>
+"""
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.``generic type names shouldn't be camelCase (multiple generic types)``() =
+        this.Parse """
+type Foo<'T1, 'T2, 'T3, 'T4, 'T5, 'a, 'T6> = Option<'T1 * 'T2 * 'T3 * 'T4 * 'T5 * 'a * 'T6>
+"""
+        Assert.IsTrue(this.ErrorsExist)
