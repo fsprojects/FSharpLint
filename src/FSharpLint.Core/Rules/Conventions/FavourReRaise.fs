46341944baed95a9268787ffbf276478bf93beda
Retheesh Erathu <sumod8898@gmail.com>
2021-10-18 16:08:00 +0200

    Add new rule FavourReRaise
    Fixes https://github.com/fsprojects/FSharpLint/issues/509

---
+++ src/FSharpLint.Core/Rules/Conventions/FavourReRaise.fs
@@ -0,0 +1,51 @@
+module FSharpLint.Rules.FavourReRaise
+
+open System
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+open FSharp.Compiler.Syntax
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+
+let private runner (args: AstNodeRuleParams) =
+    let generateError range =
+        { Range = range
+          Message = Resources.GetString "RulesFavourReRaise"
+          SuggestedFix = None
+          TypeChecks = List.empty }
+        |> Array.singleton
+
+    let rec checkExpr (expr) maybeIdent =
+        match expr with
+        | SynExpr.App (_, _, SynExpr.Ident raiseId, expression, range) when raiseId.idText = "raise" ->
+            match expression with
+            | SynExpr.Ident ident ->
+                match maybeIdent with
+                | Some id when id = ident.idText ->
+                    generateError range
+                | _ -> Array.empty
+            | SynExpr.LongIdent (_, LongIdentWithDots (id, _), _, range) -> generateError range
+            | _ -> Array.empty
+        | SynExpr.TryWith (expressions, _, clauseList, _expression, _range, _, _) as expr ->
+            clauseList
+            |> List.toArray
+            |> Array.collect (fun clause ->
+                match clause with
+                | SynMatchClause (pat, _, app, _, _) ->
+                    match pat with
+                    | SynPat.Named (_, id, _, _, _) -> checkExpr app (Some id.idText)
+                    | _ -> checkExpr app None)
+        | SynExpr.IfThenElse (_, expr, _, _, _, _, _) -> checkExpr expr maybeIdent
+        | _ -> Array.empty
+
+    match args.AstNode with
+    | AstNode.Expression expr -> checkExpr expr None
+    | _ -> Array.empty
+
+let rule =
+    { Name = "FavourReRaise"
+      Identifier = Identifiers.FavourReRaise
+      RuleConfig =
+        { AstNodeRuleConfig.Runner = runner
+          Cleanup = ignore } }
+    |> AstNodeRule
---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/FavourReRaise.fs
@@ -0,0 +1,91 @@
+module FSharpLint.Core.Tests.Rules.Conventions.FavourReRaise
+
+open NUnit.Framework
+open FSharpLint.Rules
+
+[<TestFixture>]
+type TestBindingFavourReRaise() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(FavourReRaise.rule)
+
+    [<Test>]
+    member this.``favour reraise() instead of raise``() =
+        this.Parse
+            """
+try
+    foo ()
+with
+| ex ->
+    if someCondition then
+        reraise() """
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.``using raise ex must generate error``() =
+        this.Parse
+            """
+ try
+     foo ()
+ with
+ | ex ->
+     if someCondition then
+         raise ex """
+
+        Assert.IsTrue(this.ErrorsExist)
+        Assert.IsTrue(this.ErrorExistsAt(7, 9))
+
+    [<Test>]
+    member this.``using raise outside with block must not generate error``() =
+        this.Parse
+            """
+let function1 x y =
+   try
+     try
+        if x = y then raise (InnerError("inner"))
+        else raise (OuterError("outer"))
+     with
+      | InnerError(str) -> printfn "Error1 %s" str
+   finally
+      printfn "Always print this." """
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.``using raise with an exception that's not in the with block must not generate error``() =
+        this.Parse
+            """
+try
+    foo bar
+with
+| ex ->
+    let e = ex.InnerException
+    if null <> e then
+        raise e """
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.``using raise with an exception that's not in the with block must not generate error (2)``() =
+        this.Parse
+            """
+try
+    foo bar
+with
+| ex ->
+    let e = Exception("baz", ex)
+    raise e """
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.``using raise with an exception that's not in the with block must not generate error (3)``() =
+        this.Parse
+            """
+try
+    foo bar
+with
+| e ->
+    let ex = Exception("baz", e)
+    raise ex """
+
+        this.AssertNoWarnings()
