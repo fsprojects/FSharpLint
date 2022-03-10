ac20b1f5e45fd27855d12f173f4f306e0fea19f5
ijanus <emekamicro@gmail.com>
2022-03-10 11:05:53 +0100

    FavourConsistentThis: fix false positives

--- src/FSharpLint.Core/Rules/Conventions/FavourConsistentThis.fs
+++ src/FSharpLint.Core/Rules/Conventions/FavourConsistentThis.fs
@@ -12,22 +12,28 @@ open System
 [<RequireQualifiedAccess>]
 type Config = { Symbol: string }
 
+let internal isNotConsistent identifier symbol =
+    identifier <> symbol && identifier <> "_" && identifier <> "__"
+
 let runner (config: Config) args =
     let symbol = config.Symbol
     match args.AstNode with
     | AstNode.Binding(SynBinding(_, _, _, _, _, _, _, pattern, _, _, range, _)) ->
         match pattern with
         | SynPat.LongIdent(LongIdentWithDots(identifiers, _),_, _, _, _, _) ->
-            match identifiers with
-            | head::_ when head.idText <> config.Symbol -> 
-               let error =
-                   { Range = range
-                     Message = String.Format(Resources.GetString "RulesFavourConsistentThis", config.Symbol)
-                     SuggestedFix = None
-                     TypeChecks = List.Empty }
-                   |> Array.singleton
-               error
-            | _ -> Array.empty
+            if identifiers.Length = 2 then
+                match identifiers  with
+                | head::_ when isNotConsistent head.idText symbol ->
+                    let error =
+                        { Range = range
+                          Message = String.Format(Resources.GetString "RulesFavourConsistentThis", config.Symbol)
+                          SuggestedFix = None
+                          TypeChecks = List.Empty }
+                        |> Array.singleton
+                    error
+                | _ -> Array.empty
+            else
+                Array.empty
         | _ -> Array.empty
     | _ -> Array.empty
 
