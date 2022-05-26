4580673779bf04e46c5603618ef708ddd638cdd0
Andres G. Aragoneses <knocte@gmail.com>
2022-05-26 12:18:29 +0200

    Revert "Tests: inverse GenericTypeNames tests to demonstrate issue"
    This reverts commit 8ed70d280755da4faf9a3ff59a0bc14736b7d451.

--- tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/GenericTypesNames.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/GenericTypesNames.fs
@@ -5,7 +5,7 @@ open FSharpLint.Framework.Rules
 open FSharpLint.Rules
 
 let config =
-    { NamingConfig.Naming = Some NamingCase.CamelCase
+    { NamingConfig.Naming = Some NamingCase.PascalCase
       Underscores = Some NamingUnderscores.None
       Prefix = None
       Suffix = None }
@@ -14,22 +14,22 @@ type TestConventionsGenericTypesNames() =
     inherit TestAstNodeRuleBase.TestAstNodeRuleBase(GenericTypesNames.rule config)
 
     [<Test>]
-    member this.GenericTypeNameIsCamelCase() =
+    member this.GenericTypeNameIsPascalCase() =
         this.Parse """
-type Foo<'a> = Option<'a>
+type Foo<'T> = Option<'T>
 """
         this.AssertNoWarnings()
         
     [<Test>]
-    member this.``generic type name shouldn't be PascalCase``() =
+    member this.``generic type name shouldn't be camelCase``() =
         this.Parse """
-type Foo<'T> = Option<'T>
+type Foo<'a> = Option<'a>
     """
         Assert.IsTrue(this.ErrorsExist)
         Assert.IsTrue(this.ErrorExistsOnLine 2)
 
     [<Test>]
-    member this.``generic type names shouldn't be PascalCase (2 generic types)``() =
+    member this.``generic type names shouldn't be camelCase (2 generic types)``() =
         this.Parse """
 type Foo<'a, 'T> = Option<'a * 'T>
 """
@@ -37,7 +37,7 @@ type Foo<'a, 'T> = Option<'a * 'T>
         Assert.IsTrue(this.ErrorExistsOnLine 2)
 
     [<Test>]
-    member this.``generic type names shouldn't be PascalCase (2 generic types with different order)``() =
+    member this.``generic type names shouldn't be camelCase (2 generic types with different order)``() =
         this.Parse """
 type Foo<'T, 'a> = Option<'T * 'a>
 """
@@ -45,27 +45,27 @@ type Foo<'T, 'a> = Option<'T * 'a>
         Assert.IsTrue(this.ErrorExistsOnLine 2)
 
     [<Test>]
-    member this.``generic type names are camelCase``() =
+    member this.``generic type names are PascalCase``() =
         this.Parse """
-type Foo<'k, 'v> = Option<'k * 'v>
+type Foo<'K, 'V> = Option<'K * 'V>
 """
         this.AssertNoWarnings()
 
     [<Test>]
-    member this.``generic type names shouldn't be PascalCase (multiple generic types)``() =
+    member this.``generic type names shouldn't be camelCase (multiple generic types)``() =
         this.Parse """
-type Foo<'a1, 'a2, 'a3, 'a4, 'a5, 'T, 'a6> = Option<'a1 * 'a2 * 'a3 * 'a4 * 'a5 * 'T * 'a6>
+type Foo<'T1, 'T2, 'T3, 'T4, 'T5, 'a, 'T6> = Option<'T1 * 'T2 * 'T3 * 'T4 * 'T5 * 'a * 'T6>
 """
         Assert.IsTrue(this.ErrorsExist)
         Assert.IsTrue(this.ErrorExistsOnLine 2)
 
     [<Test>]
-    member this.``generic type names shouldn't be PascalCase even for types in methods``() =
+    member this.``generic type names shouldn't be camelCase even for types in methods``() =
         this.Parse """
 module PeerChannelEncryptorMonad =
-    type PeerChannelEncryptorComputation<'a> =
+    type PeerChannelEncryptorComputation<'T> =
         | PeerChannelEncryptorComputation of
-            (PeerChannelEncryptor -> Result<'a * PeerChannelEncryptor, PeerError>)
+            (PeerChannelEncryptor -> Result<'T * PeerChannelEncryptor, PeerError>)
 
     let runP pcec initialState =
         let (PeerChannelEncryptorComputation innerFn) = pcec
@@ -78,9 +78,9 @@ module PeerChannelEncryptorMonad =
         PeerChannelEncryptorComputation innerFn
 
     let bindP
-        (f: 'T1 -> PeerChannelEncryptorComputation<'T2>)
-        (xT: PeerChannelEncryptorComputation<'T1>)
-        : PeerChannelEncryptorComputation<'T2> =
+        (f: 'a -> PeerChannelEncryptorComputation<'b>)
+        (xT: PeerChannelEncryptorComputation<'a>)
+        : PeerChannelEncryptorComputation<'b> =
         let innerFn state =
             runP xT state
             >>= fun (res, state2) ->
