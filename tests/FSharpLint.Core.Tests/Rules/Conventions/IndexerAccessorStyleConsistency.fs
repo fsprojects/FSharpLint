dcf2cc4a54aa000aefaa6ef0a46a2a8e5f6b725b
Parham Saremi <parhaamsaremi@gmail.com>
2022-07-20 16:18:18 +0430

    Add new rule IndexerAccessorStyleConsistency
    Checks for OCaml and CSharp styles. Fixes https://github.com/fsprojects/FSharpLint/issues/532

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/IndexerAccessorStyleConsistency.fs
@@ -0,0 +1,58 @@
+module FSharpLint.Core.Tests.Rules.Conventions.IndexerAccessorStyleConsistency
+
+open NUnit.Framework
+open FSharpLint.Framework.Rules
+open FSharpLint.Rules.IndexerAccessorStyleConsistency
+open FSharpLint.Rules
+
+[<TestFixture>]
+type TestConventionsIndexerAccessorStyleConsistencyCSharp() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(IndexerAccessorStyleConsistency.rule { Style = "CSharp" })
+    
+    [<Test>]
+    member this.IndexerAccessorStyleConsistencyOCamlStyleWhenUsingCSharp() =
+        this.Parse """
+module Program
+
+let someArray = [| "foo" ; "bar" |]
+let bar = someArray.[1]
+System.Console.WriteLine bar"""
+
+        Assert.IsTrue this.ErrorsExist
+    
+    [<Test>]
+    member this.IndexerAccessorStyleConsistencyCSharpStyleWhenUsingCSharp() =
+        this.Parse """
+module Program
+
+let someArray = [| "foo" ; "bar" |]
+let bar = someArray[1]
+System.Console.WriteLine bar"""
+
+        Assert.IsTrue this.NoErrorsExist
+
+[<TestFixture>]
+type TestConventionsIndexerAccessorStyleConsistencyOCaml() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(IndexerAccessorStyleConsistency.rule { Style = "OCaml" })
+
+    [<Test>]
+    member this.IndexerAccessorStyleConsistencyCSharpStyleWhenUsingOCaml() =
+        this.Parse """
+module Program
+
+let someArray = [| "foo" ; "bar" |]
+let bar = someArray[1]
+System.Console.WriteLine bar"""
+
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.IndexerAccessorStyleConsistencyOCamlStyleWhenUsingOCaml() =
+        this.Parse """
+module Program
+
+let someArray = [| "foo" ; "bar" |]
+let bar = someArray.[1]
+System.Console.WriteLine bar"""
+
+        Assert.IsTrue this.NoErrorsExist
