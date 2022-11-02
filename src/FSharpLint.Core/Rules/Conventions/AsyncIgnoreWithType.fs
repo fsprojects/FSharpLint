04ef61f089d32149add0bb617858ac346fee9280
Parham Saremi <parhaamsaremi@gmail.com>
2022-11-02 16:07:27 +0330

    Implement the rule

--- src/FSharpLint.Core/Rules/Conventions/AsyncIgnoreWithType.fs
+++ src/FSharpLint.Core/Rules/Conventions/AsyncIgnoreWithType.fs
@@ -4,10 +4,30 @@ open FSharp.Compiler.Syntax
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.AstInfo
 open FSharpLint.Framework.Rules
-
+open FSharpLint.Framework.Suggestion
 
 let runner (args:AstNodeRuleParams) =
-    Array.empty
+    let result = 
+        match args.AstNode with
+        | AstNode.Expression(SynExpr.DoBang(expr, _)) ->
+            match expr with
+            | SynExpr.App(_, _, _, ignoreExpr, _) ->
+                match ignoreExpr with
+                | SynExpr.LongIdent(_,LongIdentWithDots(ident, _), _, range) ->
+                    if ident.[0].ToString().Equals("Async") 
+                        && ident.[1].ToString().Equals("Ignore") then
+                        { Range = range
+                          Message = ""
+                          SuggestedFix = None
+                          TypeChecks = List.Empty } |> Array.singleton
+                    else 
+                        Array.empty
+                | _ -> Array.empty
+            | _ -> 
+                Array.empty
+        | _ ->
+            Array.empty
+    result
 
 let rule =
     { Name = "AsyncIgnoreWithType"
