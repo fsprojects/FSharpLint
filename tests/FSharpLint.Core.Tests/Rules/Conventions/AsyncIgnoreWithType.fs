84f7375fa154238418f0a217a85dfa4b86cdd890
Parham Saremi <parhaamsaremi@gmail.com>
2022-11-02 16:31:59 +0330

    Add new tests

--- tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncIgnoreWithType.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncIgnoreWithType.fs
@@ -17,3 +17,41 @@ module X =
     let f x = 
         do! x() |> Async.Ignore""")
         Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.AsyncIgnoreWithType() =
+        this.Parse("""
+namespace Program
+
+module X = 
+    let f x = 
+        do! x() |> Async.Ignore<int>""")
+        Assert.IsTrue this.NoErrorsExist
+
+    [<Test>]
+    member this.AsyncIgnoreComplexWithoutType() =
+        this.Parse("""
+namespace Program
+
+module X = 
+    let f x = 
+        do! 
+            x() 
+            |> UnwrapResult
+            |> Async.AwaitTask
+            |> Async.Ignore""")
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.AsyncIgnoreComplexWithtType() =
+        this.Parse("""
+namespace Program
+
+module X = 
+    let f x = 
+        do! 
+            x() 
+            |> UnwrapResult
+            |> Async.AwaitTask
+            |> Async.Ignore<int>""")
+        Assert.IsTrue this.NoErrorsExist
