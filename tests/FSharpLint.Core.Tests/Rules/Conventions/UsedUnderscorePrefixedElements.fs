8aa38399645a3ebd2be50e14e80213558c71a0a8
realmarv <zahratehraninasab@gmail.com>
2022-11-21 15:09:28 +0330

    Core.Tests: failing tests for new rule

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/UsedUnderscorePrefixedElements.fs
@@ -0,0 +1,44 @@
+module FSharpLint.Core.Tests.Rules.Conventions.UsedUnderscorePrefixedElements
+
+open NUnit.Framework
+open FSharpLint.Rules
+open System
+
+[<TestFixture>]
+type TestConventionsUsedUnderscorePrefixedElements() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(UsedUnderscorePrefixedElements.rule)
+
+    [<Test>]
+    member this.``Use variable with underscore prefix``() =
+        this.Parse """
+module MyModule =
+    let MyFunc () =
+        let _random = System.Random()
+        printfn "%A" _random
+        () """
+
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.``Not using variable with underscore prefix``() =
+        this.Parse """
+module MyModule =
+    let MyFunc () =
+        let _random = System.Random()
+        printfn "_random is not used here"
+        () """
+
+        Assert.IsFalse this.ErrorsExist
+        
+    [<Test>]
+    member this.``Using variable without underscore prefix``() =
+        this.Parse """
+module MyModule =
+    let MyFunc () =
+        let random = System.Random()
+        printfn "%A" random
+        () """
+
+        Assert.IsFalse this.ErrorsExist
+
+
