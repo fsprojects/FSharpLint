ee5907661f91ac6b6972c6470595c2f98705745f
realmarv <zahratehraninasab@gmail.com>
2022-11-22 16:02:04 +0330

    FSharpLint.Core.Tests: fix

--- tests/FSharpLint.Core.Tests/Rules/Conventions/UsedUnderscorePrefixedElements.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/UsedUnderscorePrefixedElements.fs
@@ -12,8 +12,10 @@ type TestConventionsUsedUnderscorePrefixedElementsZahra() =
     member this.``Lint flags record member(s) with type hints``() =
         this.Parse """
 module MyClass =
-    let _random = System.Random()
-    member val Random = (_random.Next() |> Some) with get, set """
+    let MyFunc =
+        let _random = System.Random()
+        printfn "%A" _random
+        () """
 
         Assert.IsTrue this.ErrorsExist
 
