1da2485f79478e1b316298935bddb44b98a85404
realmarv <zahratehraninasab@gmail.com>
2022-11-23 16:24:59 +0330

    FSharpLint.Core: refactor
    Recator in order to avoid using mutable variables

--- src/FSharpLint.Core/Rules/Conventions/UsedUnderscorePrefixedElements.fs
+++ src/FSharpLint.Core/Rules/Conventions/UsedUnderscorePrefixedElements.fs
@@ -6,46 +6,45 @@ open FSharpLint.Framework.Suggestion
 open FSharp.Compiler.Syntax
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
-
+let CheckTwice (previousIdent: Ident) (body: SynExpr) =
+     match body with
+        | SynExpr.Sequential(debugPointAtSequential, isTrueSeq, synExpr, expr2, range) ->
+            match synExpr with
+                | SynExpr.App(exprAtomicFlag, isInfix, funcExpr, argExpr, range) ->
+                    match argExpr with
+                        | SynExpr.Ident ident ->
+                            if previousIdent.idText = ident.idText then
+                                {
+                                    Range = range
+                                    Message = String.Format(Resources.GetString ("RulesUsedUnderscorePrefixedElements"))
+                                    SuggestedFix = None
+                                    TypeChecks = []
+                                } |> Array.singleton
+                            else
+                                Array.empty
+                        | _ ->
+                            Array.empty
+                | _ ->
+                    Array.empty
+        | _ ->
+                Array.empty  
 
 let runner (args:AstNodeRuleParams) =
     
-    let mutable error = Array.empty
-    let mutable idents = List.empty<string>
-    match args.AstNode with
-        | AstNode.Expression (SynExpr.LetOrUse (isRecursive, isUse, bindings, body, range)) ->
-            match bindings.[0] with
-            | SynBinding(synAccessOption, synBindingKind, mustInline, isMutable, synAttributeLists, preXmlDoc, synValData, headPat, synBindingReturnInfoOption, synExpr, range, debugPointAtBinding) ->
-                match headPat with
-                | SynPat.Named(synPat, ident, isSelfIdentifier, synAccessOption, range) ->
-                    if ident.idText.StartsWith("_") then
-                        idents <- List.append idents [ident.idText]
-                | _ ->
-                    ()
-                    
-            match body with
-            | SynExpr.Sequential(debugPointAtSequential, isTrueSeq, synExpr, expr2, range) ->
-                match synExpr with
-                    | SynExpr.App(exprAtomicFlag, isInfix, funcExpr, argExpr, range) ->
-
-                        match argExpr with
-                            | SynExpr.Ident ident ->
-                                if (List.contains ident.idText idents) then
-                                    error <- ({
-                                        Range = range
-                                        Message = String.Format(Resources.GetString ("RulesUsedUnderscorePrefixedElements"))
-                                        SuggestedFix = None
-                                        TypeChecks = []
-                                    } |> Array.singleton) 
+    let error =
+        match args.AstNode with
+            | AstNode.Expression (SynExpr.LetOrUse (isRecursive, isUse, bindings, body, range)) ->
+                match bindings.[0] with
+                    | SynBinding(synAccessOption, synBindingKind, mustInline, isMutable, synAttributeLists, preXmlDoc, synValData, headPat, synBindingReturnInfoOption, synExpr, range, debugPointAtBinding) ->
+                        match headPat with
+                            | SynPat.Named(synPat, ident, isSelfIdentifier, synAccessOption, range) ->
+                                if ident.idText.StartsWith("_") then
+                                    CheckTwice ident body
                                 else
-                                    ()
+                                    Array.empty
                             | _ ->
-                                ()
-                    | _ ->
-                        ()
-                | _ ->
-                    ()          
-        | _ -> ()
+                                Array.empty
+            | _ -> Array.empty
         
     error
 
