5fa4a58c153ab835f0d3595c70d0b3f27a028fa7
realmarv <zahratehraninasab@gmail.com>
2022-12-18 16:30:01 +0330

    FSharpLint.Core: initialize new rule

---
+++ src/FSharpLint.Core/Rules/Conventions/AvoidOnePipeOperator.fs
@@ -0,0 +1,21 @@
+module FSharpLint.Rules.AvoidOnePipeOperator
+
+open System
+
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+open FSharp.Compiler.Syntax
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+
+let runner (args: AstNodeRuleParams) =
+    
+    let error = Array.empty
+      
+    error
+
+let rule =
+    { Name = "AvoidOnePipeOperator"
+      Identifier = Identifiers.AvoidOnePipeOperator
+      RuleConfig = { AstNodeRuleConfig.Runner = runner; Cleanup = ignore } }
+    |> AstNodeRule
