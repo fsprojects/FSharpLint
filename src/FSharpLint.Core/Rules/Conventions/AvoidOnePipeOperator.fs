ad90c0a808b115b9a784e0f915af74d24d63a77a
realmarv <zahratehraninasab@gmail.com>
2022-12-18 18:21:13 +0330

    FSharpLint.Core: implement new rule

--- src/FSharpLint.Core/Rules/Conventions/AvoidOnePipeOperator.fs
+++ src/FSharpLint.Core/Rules/Conventions/AvoidOnePipeOperator.fs
@@ -9,9 +9,30 @@ open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
 let runner (args: AstNodeRuleParams) =
-    printfn "%A" args.AstNode
-    let error = Array.empty
-      
+    
+    let error =
+        match args.AstNode with
+        | AstNode.Binding (SynBinding(synAccessOption, synBindingKind, mustInline, isMutable, synAttributeLists, preXmlDoc, synValData, headPat, synBindingReturnInfoOption, synExpr, range, debugPointAtBinding)) ->
+            match synExpr with
+            | SynExpr.App(exprAtomicFlag, isInfix, funcExpr, argExpr, range) ->
+                match funcExpr with
+                | SynExpr.App(exprAtomicFlag, isInfix, funcExpr, argExpr, range) ->
+                    match funcExpr with
+                    | SynExpr.Ident ident ->
+                        if ident.idText = "op_PipeRight" then
+                            {
+                                Range = range
+                                Message = String.Format(Resources.GetString ("RulesAvoidOnePipeOperator"))
+                                SuggestedFix = None
+                                TypeChecks = List.Empty
+                            } |> Array.singleton
+                        else
+                            Array.empty
+                    | _ -> Array.empty
+                | _ -> Array.empty
+            | _ -> Array.empty
+        | _ -> Array.empty
+    
     error
 
 let rule =
