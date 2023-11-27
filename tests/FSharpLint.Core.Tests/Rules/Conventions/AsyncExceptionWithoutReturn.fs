67ac4b46360e773d4f260dd5b7da53eda06b4d21
Mehrshad <code.rezaei@gmail.com>
2023-11-27 16:17:44 +0330

    FSharpLint.Core.Test: add a failure test
    Co-authored-by: webwarrior <reg@webwarrior.ws>

--- tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncExceptionWithoutReturn.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncExceptionWithoutReturn.fs
@@ -56,3 +56,18 @@ let someAsyncFunction = async {
     }""" ident)
 
         this.AssertNoWarnings()
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturnInnerExpression() = 
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction =
+    async {
+        if 2 = 2 then
+            raise (new System.Exception("An error occurred."))
+
+        return true
+    }""")
+
+        Assert.IsTrue this.ErrorsExist
