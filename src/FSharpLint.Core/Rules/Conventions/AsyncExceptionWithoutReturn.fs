d4bc4379b2534bb9f609dc5483e870f79deb01eb
Mehrshad <code.rezaei@gmail.com>
2023-11-27 14:33:43 +0330

    WIP: implement the rule for simple case
    Co-authored-by: webwarrior <reg@webwarrior.ws>

--- src/FSharpLint.Core/Rules/Conventions/AsyncExceptionWithoutReturn.fs
+++ src/FSharpLint.Core/Rules/Conventions/AsyncExceptionWithoutReturn.fs
@@ -7,27 +7,36 @@ open FSharp.Compiler.Text
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
-let checkAsyncExceptionWithoutReturn (args:AstNodeRuleParams) (range:Range) (doBangExpr:SynExpr) breadcrumbs =
-    //match node with
-    //    | SynExpr.App (_, _, SynExpr.Ident failwithId, expression, range) when
-    //        failwithId.idText = "failwith"
-    //        || failwithId.idText = "failwithf"
-    //        ->
-    //        match expression with
-    //match node with 
-    //    | SynExpr.App (_, _, _, _, SynExpr.CompExpr) when
-                
-
 let runner args =
-    printf "\n________%A" args.AstNode
     match args.AstNode with
-    | AstNode.Expression (SynExpr.DoBang (expr, range)) ->
-        let parents = args.GetParents 5
-        checkAsyncExceptionWithoutReturn args range expr parents
+    | AstNode.Expression(
+    SynExpr.App (_, 
+    _, 
+    (SynExpr.Ident compExprName), 
+    (SynExpr.CompExpr (_, _, innerExpression, _)),
+    range))
+        when compExprName.idText = "async" ->
+        match innerExpression with
+        | SynExpr.Sequential(_, _, expression, _, _) -> 
+            match expression with
+            | SynExpr.App (_, _, SynExpr.Ident failwithId, _, _) when
+                failwithId.idText = "failwith"
+                || failwithId.idText = "failwithf"
+                || failwithId.idText = "raise" ->
+                    {   Range = range
+                        Message = Resources.GetString "RulesAsyncExceptionWithoutReturn"
+                        SuggestedFix = None
+                        TypeChecks = List.Empty
+                    } |> Array.singleton
+            | _ -> Array.empty
+        | _ -> Array.empty
     | _ -> Array.empty
 
 let rule =
     { Name = "AsyncExceptionWithoutReturn"
       Identifier = Identifiers.AsyncExceptionWithoutReturn
       RuleConfig = { AstNodeRuleConfig.Runner = runner; Cleanup = ignore } }
+      RuleConfig =
+        { AstNodeRuleConfig.Runner = runner
+          Cleanup = ignore } }
     |> AstNodeRule
--- tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncExceptionWithoutReturn.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncExceptionWithoutReturn.fs
@@ -9,24 +9,51 @@ type TestAsyncExceptionWithoutReturn() =
     inherit TestAstNodeRuleBase.TestAstNodeRuleBase(AsyncExceptionWithoutReturn.rule)
 
     [<Test>]
-    member this.abcdefg1() = 
+    member this.AsyncExceptionWithoutReturn() = 
         this.Parse("""
 namespace Program
 
 let someAsyncFunction = async {
     raise (new System.Exception("An error occurred."))
-    true
+    return true
     }""")
 
         Assert.IsTrue this.ErrorsExist
 
     [<Test>]
-    member this.abcdefg2() = 
+    member this.AsyncExceptionWithoutReturn_2() = 
         this.Parse("""
 namespace Program
 
 let someAsyncFunction = async {
     return raise (new System.Exception("An error occurred."))
     }""")
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturnOnFailWith() = 
+        for ident in ["failwith"; "failwithf"] do
+            
+            this.Parse(sprintf """
+namespace Program
+
+let someAsyncFunction = async {
+    %s "An error occurred."
+    return true
+    }""" ident)
+
+            Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturn_4() = 
+    member this.AsyncExceptionWithoutReturnOnFailWith_2() = 
+        for ident in ["failwith"; "failwithf"] do
+            this.Parse(sprintf """
+namespace Program
+
+let someAsyncFunction = async {
+    return %s "An error occurred."
+    }""" ident)
 
         this.AssertNoWarnings()
