80bfd15a2347ac7d8bcc25df3b5be494c473db2f
Mehrshad <code.rezaei@gmail.com>
2023-11-28 12:30:46 +0330

    FSharpLint.Core: apply rule for inner expressions
    Co-authored-by: webwarrior <reg@webwarrior.ws>

--- src/FSharpLint.Core/Rules/Conventions/AsyncExceptionWithoutReturn.fs
+++ src/FSharpLint.Core/Rules/Conventions/AsyncExceptionWithoutReturn.fs
@@ -7,24 +7,57 @@ open FSharp.Compiler.Text
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
+let rec checkExpression (expression: SynExpr) (range: range) =
+    match expression with
+    | SynExpr.Sequential (_, _, firstExpression, secondExpression, _) ->
+        let result = checkExpression firstExpression range
+        Array.append result (checkExpression secondExpression secondExpression.Range)
+    | SynExpr.Paren (innerExpression, _, _, range) -> checkExpression innerExpression range
+    | SynExpr.While (_, _, innerExpression, range) -> checkExpression innerExpression range
+    | SynExpr.For (_, _, _, _, _, innerExpression, range) -> checkExpression innerExpression range
+    | SynExpr.ForEach (_, _, _, _, _, innerExpression, range) -> checkExpression innerExpression range
+    | SynExpr.Match (_, _, clauses, range) ->
+        clauses
+        |> List.map (fun (SynMatchClause (_, _, clause, range, _)) -> checkExpression clause range)
+        |> List.toArray
+        |> Array.concat
+    | SynExpr.Do (innerExpression, range) -> checkExpression innerExpression range
+    | SynExpr.TryWith (tryExpression, tryRange, withCases, _, _, _, _) ->
+        withCases
+        |> List.map (fun (SynMatchClause (_, _, withCase, withRange, _)) -> checkExpression withCase withRange)
+        |> List.toArray
+        |> Array.concat
+        |> Array.append (checkExpression tryExpression tryRange)
+    | SynExpr.TryFinally (tryExpression, finallyExpr, range, _, _) ->
+        checkExpression finallyExpr range
+        |> Array.append (checkExpression tryExpression range)
+    | SynExpr.IfThenElse (_, thenExpr, elseExpr, _, _, _, range) ->
+        let checkThen = checkExpression thenExpr range
+
+        match elseExpr with
+        | Some elseExpression ->
+            checkThen
+            |> Array.append (checkExpression elseExpression range)
+        | None -> checkThen
+    | SynExpr.App (_, _, SynExpr.Ident failwithId, _, _) when
+        failwithId.idText = "failwith"
+        || failwithId.idText = "failwithf"
+        || failwithId.idText = "raise"
+        ->
+        { Range = range
+          Message = Resources.GetString "RulesAsyncExceptionWithoutReturn"
+          SuggestedFix = None
+          TypeChecks = List.Empty }
+        |> Array.singleton
+    | _ -> Array.empty
+
+
 let runner args =
     match args.AstNode with
-    | AstNode.Expression(SynExpr.App (_, _, (SynExpr.Ident compExprName), (SynExpr.CompExpr (_, _, innerExpression, _)), range))
-        when compExprName.idText = "async" ->
-        match innerExpression with
-        | SynExpr.Sequential(_, _, expression, _, _) -> 
-            match expression with
-            | SynExpr.App (_, _, SynExpr.Ident failwithId, _, _) when
-                failwithId.idText = "failwith"
-                || failwithId.idText = "failwithf"
-                || failwithId.idText = "raise" ->
-                    {   Range = range
-                        Message = Resources.GetString "RulesAsyncExceptionWithoutReturn"
-                        SuggestedFix = None
-                        TypeChecks = List.Empty
-                    } |> Array.singleton
-            | _ -> Array.empty
-        | _ -> Array.empty
+    | AstNode.Expression
+        (
+            SynExpr.App (_, _, (SynExpr.Ident compExprName), (SynExpr.CompExpr (_, _, innerExpression, _)), range)
+        ) when compExprName.idText = "async" -> checkExpression innerExpression range
     | _ -> Array.empty
 
 let rule =
