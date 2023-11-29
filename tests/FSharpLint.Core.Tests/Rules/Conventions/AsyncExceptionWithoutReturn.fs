aecdf1d47e22864cfe7126b2c9fe628b4e5495fd
Mehrshad <code.rezaei@gmail.com>
2023-11-29 12:56:28 +0330

    FSharpLint.Core.Test: separate failwith/failwithf
    The commit splits the failwith and failwithf tests
    and improves them.

--- tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncExceptionWithoutReturn.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncExceptionWithoutReturn.fs
@@ -9,56 +9,112 @@ type TestAsyncExceptionWithoutReturn() =
     inherit TestAstNodeRuleBase.TestAstNodeRuleBase(AsyncExceptionWithoutReturn.rule)
 
     [<Test>]
-    member this.AsyncExceptionWithoutReturn() = 
+    member this.AsyncRaiseWithoutReturn() = 
         this.Parse("""
 namespace Program
 
-let someAsyncFunction = async {
-    raise (new System.Exception("An error occurred."))
-    return true
+let someAsyncFunction =
+    async {
+        raise (new System.Exception("An error occurred."))
+        return true
     }""")
 
         Assert.IsTrue this.ErrorsExist
 
     [<Test>]
-    member this.AsyncExceptionWithoutReturn_2() = 
+    member this.AsyncRaiseWithReturn() = 
         this.Parse("""
 namespace Program
 
-let someAsyncFunction = async {
-    return raise (new System.Exception("An error occurred."))
+let someAsyncFunction =
+    async {
+        return raise (new System.Exception("An error occurred."))
     }""")
 
         this.AssertNoWarnings()
 
     [<Test>]
-    member this.AsyncExceptionWithoutReturnOnFailWith() = 
-        for ident in ["failwith"; "failwithf"] do
-            
-            this.Parse(sprintf """
+    member this.AsyncFailWithWithoutReturn() =          
+        this.Parse("""
 namespace Program
 
-let someAsyncFunction = async {
-    %s "An error occurred."
-    return true
-    }""" ident)
+let someAsyncFunction =
+    async {
+        failwith "An error occurred."
+        return true
+    }""")
 
-            Assert.IsTrue this.ErrorsExist
+        Assert.IsTrue this.ErrorsExist
 
     [<Test>]
-    member this.AsyncExceptionWithoutReturnOnFailWith_2() = 
-        for ident in ["failwith"; "failwithf"] do
-            this.Parse(sprintf """
+    member this.AsyncFailwithfWithoutReturn_1() =          
+        this.Parse("""
 namespace Program
 
-let someAsyncFunction = async {
-    return %s "An error occurred."
-    }""" ident)
+let someAsyncFunction =
+    async {
+        let errCode = 78
+        failwithf "Dummy Error Message: %i" errCode
+        return true
+    }""")
+
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.AsyncFailwithfWithoutReturn_2() =          
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction =
+    async {
+        let errCode = 78
+        failwithf "Dummy Error Message: %i" errCode
+    }""")
+
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.AsyncFailwithWithReturn() = 
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction =
+    async {
+        return failwith "An error occurred."
+    }""")
 
         this.AssertNoWarnings()
 
     [<Test>]
-    member this.AsyncExceptionWithoutReturnInnerExpression() = 
+    member this.AsyncFailwithfWithReturn() = 
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction =
+    async {
+        let errCode = 78
+        return failwithf "Dummy Error Message: %i" errCode
+    }""")
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.AsyncRaiseWithReturnInnerExpression() = 
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction =
+    async {
+        if 2 = 2 then
+            return raise (new System.Exception("An error occurred."))
+
+        return true
+    }""")
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.AsyncRaiseWithoutReturnInnerExpression() = 
         this.Parse("""
 namespace Program
 
