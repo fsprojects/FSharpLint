a31c9198c07b937b2f591ecf63fc55e4fbb32875
Mehrshad <code.rezaei@gmail.com>
2023-12-13 16:01:25 +0330

    build: run FSharpLint with 1 extra rule

--- build.fsx
+++ build.fsx
@@ -287,6 +287,7 @@ Target.create "SelfCheck" (fun _ ->
             "favourTypedIgnore"
             "favourStaticEmptyFields"
             "favourConsistentThis"
+            "avoidTooShortNames"
         ]
 
     let jsonObj = JObject.Parse fsharplintJsonText
