f6e0fba6a9bc8cf61a92c03570130e2cedbeea5a
webwarrior <reg@webwarrior.ws>
2023-12-21 13:36:34 +0100

    Core(Tests): add FavourNestedFunctions rule
    Added FavourNestedFunctions rule and tests for it.

---
+++ src/FSharpLint.Core/Rules/Conventions/FavourNestedFunctions.fs
@@ -0,0 +1,19 @@
+﻿module FSharpLint.Rules.FavourNestedFunctions
+
+open System
+open FSharp.Compiler.Syntax
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+
+let runner (args: AstNodeRuleParams) =
+    failwith "Not yet implemeted"    
+
+let rule =
+    { Name = "FavourNestedFunctions"
+      Identifier = Identifiers.FavourNestedFunctions
+      RuleConfig =
+        { AstNodeRuleConfig.Runner = runner
+          Cleanup = ignore } }
+    |> AstNodeRule
