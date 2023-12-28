994a8105a0f6d41a9ebf46cae4b3f6b0d5dfd20c
webwarrior <reg@webwarrior.ws>
2023-12-28 13:15:19 +0100

    EnsureTailCallDiagnosticsInRecursiveFunctions, Tests: add rule and tests
    Add EnsureTailCallDiagnosticsInRecursiveFunctions rule and tests for it.

---
+++ src/FSharpLint.Core/Rules/Conventions/EnsureTailCallDiagnosticsInRecursiveFunctions.fs
@@ -0,0 +1,20 @@
+﻿module FSharpLint.Rules.EnsureTailCallDiagnosticsInRecursiveFunctions
+
+open System
+
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+open FSharp.Compiler.Syntax
+open FSharp.Compiler.Text
+open FSharp.Compiler.CodeAnalysis
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+
+let runner (args: AstNodeRuleParams) =
+    failwith "Not yet implemented"
+
+let rule =
+    { Name = "EnsureTailCallDiagnosticsInRecursiveFunctions"
+      Identifier = Identifiers.EnsureTailCallDiagnosticsInRecursiveFunctions
+      RuleConfig = { AstNodeRuleConfig.Runner = runner; Cleanup = ignore } }
+    |> AstNodeRule
