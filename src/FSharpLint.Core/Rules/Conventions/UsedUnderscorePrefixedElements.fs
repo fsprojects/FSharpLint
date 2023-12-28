078fbf91ac9d2a83c873fb72436984462b5b6aee
realmarv <zahratehraninasab@gmail.com>
2023-12-28 11:49:39 +0800

    UsedUnderscorePrefixedElements: make tests pass

--- src/FSharpLint.Core/Rules/Conventions/UsedUnderscorePrefixedElements.fs
+++ src/FSharpLint.Core/Rules/Conventions/UsedUnderscorePrefixedElements.fs
@@ -8,9 +8,46 @@ open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
 
-
 let runner (args:AstNodeRuleParams) =
-    Array.empty
+    
+    let mutable error = Array.empty
+    let mutable idents = List.empty<string>
+    match args.AstNode with
+        | AstNode.Expression (SynExpr.LetOrUse (isRecursive, isUse, bindings, body, range)) ->
+            match bindings.[0] with
+            | SynBinding(synAccessOption, synBindingKind, mustInline, isMutable, synAttributeLists, preXmlDoc, synValData, headPat, synBindingReturnInfoOption, synExpr, range, debugPointAtBinding) ->
+                match headPat with
+                | SynPat.Named(synPat, ident, isSelfIdentifier, synAccessOption, range) ->
+                    if ident.idText.StartsWith("_") then
+                        idents <- List.append idents [ident.idText]
+                | _ ->
+                    ()
+                    
+            match body with
+            | SynExpr.Sequential(debugPointAtSequential, isTrueSeq, synExpr, expr2, range) ->
+                match synExpr with
+                    | SynExpr.App(exprAtomicFlag, isInfix, funcExpr, argExpr, range) ->
+
+                        match argExpr with
+                            | SynExpr.Ident ident ->
+                                if (List.contains ident.idText idents) then
+                                    error <- ({
+                                        Range = range
+                                        Message = String.Format(Resources.GetString ("RulesUsedUnderscorePrefixedElements"))
+                                        SuggestedFix = None
+                                        TypeChecks = []
+                                    } |> Array.singleton) 
+                                else
+                                    ()
+                            | _ ->
+                                ()
+                    | _ ->
+                        ()
+                | _ ->
+                    ()          
+        | _ -> ()
+        
+    error
 
 let rule =
     { Name = "UsedUnderscorePrefixedElements"
