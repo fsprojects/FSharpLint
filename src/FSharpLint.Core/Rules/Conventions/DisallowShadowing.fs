fd82f147ffbc9a0307a8c1ca5a3543d8448c1759
webwarrior <reg@webwarrior.ws>
2024-01-04 11:45:38 +0100

    Core(Tests): introduce DisallowShadowing rule
    Added DisallowShadowing rule (no implementation yet). Added
    tests for it.

---
+++ src/FSharpLint.Core/Rules/Conventions/DisallowShadowing.fs
@@ -0,0 +1,21 @@
+﻿module FSharpLint.Rules.DisallowShadowing
+
+open System
+
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+open FSharp.Compiler.Syntax
+open FSharp.Compiler.Text
+open FSharp.Compiler.CodeAnalysis
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+
+let runner (args: AstNodeRuleParams) =
+    
+    failwith "Not yet implemented"
+
+let rule =
+    { Name = "DisallowShadowing"
+      Identifier = Identifiers.DisallowShadowing
+      RuleConfig = { AstNodeRuleConfig.Runner = runner; Cleanup = ignore } }
+    |> AstNodeRule
