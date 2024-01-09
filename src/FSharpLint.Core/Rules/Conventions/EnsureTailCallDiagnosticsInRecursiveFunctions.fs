193f81f44b0aeaf3941e15e71e75654d5b7eb154
webwarrior <reg@webwarrior.ws>
2024-01-09 12:06:55 +0100

    Core: refactoring
    Refactored code of UnneededRecKeyword and
    EnsureTailCallDiagnosticsInRecursiveFunctions rules by
    extracting common functionality.

--- src/FSharpLint.Core/Rules/Conventions/EnsureTailCallDiagnosticsInRecursiveFunctions.fs
+++ src/FSharpLint.Core/Rules/Conventions/EnsureTailCallDiagnosticsInRecursiveFunctions.fs
@@ -5,57 +5,38 @@ open System
 open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 open FSharp.Compiler.Syntax
-open FSharp.Compiler.Text
-open FSharp.Compiler.CodeAnalysis
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
+let private emitWarning (func: UnneededRecKeyword.RecursiveFunctionInfo) =
+    { Range = func.Range
+      Message =
+        String.Format(
+            Resources.GetString "RulesEnsureTailCallDiagnosticsInRecursiveFunctions",
+            func.Identifier.idText
+        )
+      SuggestedFix = None
+      TypeChecks = list.Empty }
+
 let runner (args: AstNodeRuleParams) =
     match args.AstNode, args.CheckInfo with
-    | AstNode.ModuleDeclaration (SynModuleDecl.Let (true, bindings, _)), Some checkInfo ->
-        match bindings with
-        | SynBinding (_, _, _, _, attributes, _, _, SynPat.LongIdent (SynLongIdent([ident], _, _), _, _, _, _, range), _, body, _, _, _) :: _ ->
-            let funcName = ident.idText
-        
-            let functionCallsItself =
-                checkInfo.GetAllUsesOfAllSymbolsInFile()
-                |> Seq.exists (fun usage -> 
-                    usage.Symbol.DisplayName = funcName 
-                    && ExpressionUtilities.rangeContainsOtherRange body.Range usage.Range)
-
-            if functionCallsItself then
-                let hasTailCallAttribute =
-                    attributes 
-                    |> List.collect (fun attrs -> attrs.Attributes) 
-                    |> List.exists 
-                        (fun attr -> 
-                            match attr.TypeName with
-                            | SynLongIdent([ident], _, _) ->
-                                ident.idText = "TailCall" || ident.idText = "TailCallAttribute"
-                            | _ -> false)
-                if hasTailCallAttribute then
-                    Array.empty
-                else
-                    { Range = range
-                      Message =
-                        String.Format(
-                            Resources.GetString "RulesEnsureTailCallDiagnosticsInRecursiveFunctions",
-                            funcName
-                        )
-                      SuggestedFix = None
-                      TypeChecks = list.Empty }
-                    |> Array.singleton
+    | UnneededRecKeyword.RecursiveFunction(func), Some checkInfo ->
+        if UnneededRecKeyword.functionCallsItself checkInfo func then
+            let hasTailCallAttribute =
+                func.Attributes 
+                |> List.collect (fun attrs -> attrs.Attributes) 
+                |> List.exists 
+                    (fun attr -> 
+                        match attr.TypeName with
+                        | SynLongIdent([ident], _, _) ->
+                            ident.idText = "TailCall" || ident.idText = "TailCallAttribute"
+                        | _ -> false)
+            if hasTailCallAttribute then
+                Array.empty
             else
-                { Range = range
-                  Message =
-                    String.Format(
-                        Resources.GetString "RulesUnneededRecKeyword",
-                        funcName
-                    )
-                  SuggestedFix = None
-                  TypeChecks = list.Empty }
-                |> Array.singleton
-        | _ -> Array.empty
+                emitWarning func |> Array.singleton
+        else
+            UnneededRecKeyword.emitWarning func |> Array.singleton
     | _ -> Array.empty
 
 let rule =
--- src/FSharpLint.Core/Rules/Conventions/UnneededRecKeyword.fs
+++ src/FSharpLint.Core/Rules/Conventions/UnneededRecKeyword.fs
@@ -2,41 +2,51 @@
 
 open System
 open FSharp.Compiler.Syntax
+open FSharp.Compiler.CodeAnalysis
+open FSharp.Compiler.Text
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 
+type internal RecursiveFunctionInfo =
+    {
+        Identifier: Ident
+        Range: range
+        Body: SynExpr
+        Attributes: SynAttributes
+    }
+
+let internal (|RecursiveFunction|_|) (astNode: AstNode)  =
+    match astNode with
+    | AstNode.ModuleDeclaration (SynModuleDecl.Let (true, bindings, _)) ->
+        match bindings with
+        | SynBinding (_, _, _, _, attributes, _, _, SynPat.LongIdent (SynLongIdent([ident], _, _), _, _, _, _, range), _, body, _, _, _) :: _ ->
+            Some { Identifier = ident; Range = range; Body = body; Attributes = attributes } 
+        | _ -> None
+    | _ -> None
+
+let internal functionCallsItself (checkInfo: FSharpCheckFileResults) (func: RecursiveFunctionInfo) =
+    let funcName = func.Identifier.idText
+    checkInfo.GetAllUsesOfAllSymbolsInFile()
+    |> Seq.exists (fun usage -> 
+        usage.Symbol.DisplayName = funcName 
+        && ExpressionUtilities.rangeContainsOtherRange func.Body.Range usage.Range)
+
+let internal emitWarning (func: RecursiveFunctionInfo) =
+    { Range = func.Range
+      Message =
+          String.Format(
+              Resources.GetString "RulesUnneededRecKeyword",
+              func.Identifier.idText
+          )
+      SuggestedFix = None
+      TypeChecks = list.Empty }
+
 let runner (args: AstNodeRuleParams) =
     match args.AstNode, args.CheckInfo with
-    | AstNode.ModuleDeclaration (SynModuleDecl.Let (isRecursive, bindings, letRange)), Some checkInfo when isRecursive ->
-        match bindings with
-        | SynBinding (_, _, _, _, _, _, _, SynPat.LongIdent (SynLongIdent([ident], _, _), _, _, _, _, range), _, _, _, _, _) :: _ ->
-            let symbolUses = checkInfo.GetAllUsesOfAllSymbolsInFile()
-            let funcName = ident.idText
-
-            let functionCalls =
-                symbolUses
-                |> Seq.filter (fun usage ->
-                    usage.Symbol.DisplayName = funcName
-                    && usage.Range.StartLine >= letRange.StartLine
-                    && usage.Range.EndLine <= letRange.EndLine)
-
-
-            if (functionCalls |> Seq.length) <= 1 then
-                { Range = range
-                  Message =
-                    String.Format(
-                        Resources.GetString "RulesUnneededRecKeyword",
-                        funcName
-                    )
-                  SuggestedFix = None
-                  TypeChecks = list.Empty }
-                |> Array.singleton
-            else
-                Array.empty
-        | _ -> Array.empty
-
+    | RecursiveFunction(func), Some checkInfo when not (functionCallsItself checkInfo func) ->
+        emitWarning func |> Array.singleton
     | _ -> Array.empty
 
 let rule =
