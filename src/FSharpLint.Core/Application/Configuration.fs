306e62db74fc2a912cdd2908eea4f11288bf9014
webwarrior <reg@webwarrior.ws>
2024-01-09 10:32:02 +0100

    DisallowShadowing: docs and config
    Added config and docs for DisallowShadowing rule.

--- src/FSharpLint.Core/Application/Configuration.fs
+++ src/FSharpLint.Core/Application/Configuration.fs
@@ -324,7 +324,8 @@ type ConventionsConfig =
       favourReRaise:EnabledConfig option
       favourConsistentThis:RuleConfig<FavourConsistentThis.Config> option
       suggestUseAutoProperty:EnabledConfig option
-      usedUnderscorePrefixedElements:EnabledConfig option }
+      usedUnderscorePrefixedElements:EnabledConfig option
+      disallowShadowing:EnabledConfig option }
 with
     member this.Flatten() =
         [|
@@ -348,6 +349,7 @@ with
             this.numberOfItems |> Option.map (fun config -> config.Flatten()) |> Option.toArray |> Array.concat
             this.binding |> Option.map (fun config -> config.Flatten()) |> Option.toArray |> Array.concat
             this.suggestUseAutoProperty |> Option.bind (constructRuleIfEnabled SuggestUseAutoProperty.rule) |> Option.toArray
+            this.disallowShadowing |> Option.bind (constructRuleIfEnabled DisallowShadowing.rule) |> Option.toArray
         |] |> Array.concat
 
 type TypographyConfig =
@@ -469,7 +471,8 @@ type Configuration =
       TrailingNewLineInFile:EnabledConfig option
       NoTabCharacters:EnabledConfig option
       NoPartialFunctions:RuleConfig<NoPartialFunctions.Config> option
-      SuggestUseAutoProperty:EnabledConfig option }
+      SuggestUseAutoProperty:EnabledConfig option
+      DisallowShadowing:EnabledConfig option }
 with
     static member Zero = {
         Global = None
@@ -559,6 +562,7 @@ with
         NoTabCharacters = None
         NoPartialFunctions = None
         SuggestUseAutoProperty = None
+        DisallowShadowing = None
     }
 
 // fsharplint:enable RecordFieldNames
@@ -711,6 +715,7 @@ let flattenConfig (config:Configuration) =
             config.TrailingNewLineInFile |> Option.bind (constructRuleIfEnabled TrailingNewLineInFile.rule)
             config.NoTabCharacters |> Option.bind (constructRuleIfEnabled NoTabCharacters.rule)
             config.NoPartialFunctions |> Option.bind (constructRuleWithConfig NoPartialFunctions.rule)
+            config.DisallowShadowing |> Option.bind (constructRuleIfEnabled DisallowShadowing.rule)
         |] |> Array.choose id
 
     if config.NonPublicValuesNames.IsSome &&
