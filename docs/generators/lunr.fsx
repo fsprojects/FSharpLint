9a5f3936b850ba6b7d4688c1861a4458e62f0e98
Richard Webb <richard.webb@helpsystems.com>
2025-07-04 00:40:39 +0100

    WIP: Try to get the docs building with NuGet instead of Paket

--- docs/generators/lunr.fsx
+++ docs/generators/lunr.fsx
@@ -1,6 +1,6 @@
 #r "../_lib/Fornax.Core.dll"
-#r "../../packages/docs/Newtonsoft.Json/lib/netstandard2.0/Newtonsoft.Json.dll"
-#r "../../packages/docs/FSharp.Formatting/lib/netstandard2.0/FSharp.MetadataFormat.dll"
+#r "../../packages/docs/newtonsoft.json/13.0.3/lib/netstandard2.0/Newtonsoft.Json.dll"
+#r "../../packages/docs/fsharp.formatting/4.1.0/lib/netstandard2.0/FSharp.MetadataFormat.dll"
 #if !FORNAX
 #load "../loaders/contentloader.fsx"
 #load "../loaders/apirefloader.fsx"
