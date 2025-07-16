f6ee9713a918b1d1e828bda2ee8a755b6d139d55
Andrii Chebukin <XperiAndri@Outlook.com>
2025-07-16 13:05:35 +0300

    Update src/FSharpLint.Console/Program.fs

--- src/FSharpLint.Console/Program.fs
+++ src/FSharpLint.Console/Program.fs
@@ -86,6 +86,7 @@ let private start (arguments:ParseResults<ToolArgs>) (toolsPath:Ionide.ProjInfo.
             |> Seq.pick (function | :? AssemblyInformationalVersionAttribute as aiva -> Some aiva.InformationalVersion | _ -> None)
         $"Current version: {version}" |> output.WriteInfo
         Environment.Exit 0
+
     let handleError (str:string) =
         output.WriteError str
         exitCode <- -1
