999d239df2da8d8cc74270d2100227f5bbbed720
Andres G. Aragoneses <knocte@gmail.com>
2025-08-12 14:30:02 +0200

    Tests: add quickfix from camelCase to snake_case

--- tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/LiteralNames.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/LiteralNames.fs
@@ -124,3 +124,24 @@ let SuperCat = 5
 
         Assert.IsTrue(this.ErrorExistsOnLine 5)
 
+    [<Test>]
+    member this.SuggestedFix() =
+        let source = """
+module Program
+
+[<Literal>]
+let superCat = 5
+"""
+        let expected = """
+module Program
+
+[<Literal>]
+let super_cat = 5
+"""
+        this.Parse source
+
+        Assert.IsTrue(this.ErrorExistsOnLine 5)
+
+        let result = this.ApplyQuickFix source
+
+        Assert.AreEqual(expected, result)
