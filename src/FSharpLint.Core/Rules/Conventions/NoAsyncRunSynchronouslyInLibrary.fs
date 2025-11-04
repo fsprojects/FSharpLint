69dc2d9a57612ac0ff5860e13332bd74210691be
webwarrior-ws <reg@webwarrior.ws>
2025-11-04 12:32:31 +0100

    NoAsyncRunSynchronouslyInLibrary: implement rule
    No checks for assembly name yet.

--- src/FSharpLint.Core/Rules/Conventions/NoAsyncRunSynchronouslyInLibrary.fs
+++ src/FSharpLint.Core/Rules/Conventions/NoAsyncRunSynchronouslyInLibrary.fs
@@ -8,8 +8,34 @@ open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 open FSharpLint.Framework.Utilities
 
+let hasEntryPointAttribute (syntaxArray: array<AbstractSyntaxArray.Node>) =
+    let hasEntryPoint (attrs: SynAttributeList) =
+        attrs.Attributes 
+        |> List.exists 
+            (fun attr -> 
+                match attr.TypeName with
+                | SynLongIdent([ident], _, _) -> ident.idText = "EntryPoint"
+                | _ -> false)
+
+    syntaxArray
+    |> Array.exists 
+        (fun node -> 
+            match node.Actual with
+            | AstNode.Binding(SynBinding(_, _, _, _, attributes, _, _, _, _, _, _, _, _)) -> 
+                attributes |> List.exists hasEntryPoint
+            | _ -> false)
+
 let checkIfInLibrary (syntaxArray: array<AbstractSyntaxArray.Node>) (range: range) : array<WarningDetails> =
-    failwith "Not implemented"
+    if hasEntryPointAttribute syntaxArray then
+        Array.empty
+    else
+        Array.singleton 
+            { 
+                Range = range
+                Message = Resources.GetString "NoAsyncRunSynchronouslyInLibrary"
+                SuggestedFix = None
+                TypeChecks = List.Empty 
+            }
 
 let runner args =
     match args.AstNode with
