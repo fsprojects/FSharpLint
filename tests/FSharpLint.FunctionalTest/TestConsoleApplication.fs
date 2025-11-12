5c3911198096f9a1338ccbc9f313b19d32221786
webwarrior-ws <reg@webwarrior.ws>
2025-11-12 11:56:02 +0100

    Console,FunctionalTest,docs,ReadMe,CI: upd tool name
    Change assembly name of FSharpLint.Console project from
    the (too long & convolutted) `dotnet-fsharplint` to the
    original `fsharplint`.
    
    This way it will be published as fsharplint nuget package
    and the tool name will be just fsharplint, not
    dotnet-fsharplint.

--- tests/FSharpLint.FunctionalTest/TestConsoleApplication.fs
+++ tests/FSharpLint.FunctionalTest/TestConsoleApplication.fs
@@ -39,7 +39,7 @@ module Tests =
                 |> Seq.sortByDescending _.Name
                 |> Seq.tryHead
             match dllDir with
-            | Some dir -> dir.FullName </> "dotnet-fsharplint.dll"
+            | Some dir -> dir.FullName </> "fsharplint.dll"
             | None -> failwithf "No target framework folder found in %s" binDir.FullName
 
         let startInfo = ProcessStartInfo
