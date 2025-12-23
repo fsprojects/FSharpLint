1f03da55e27592b4b4320bf393ff58dfae4fa50f
webwarrior-ws <reg@webwarrior.ws>
2025-12-23 09:50:29 +0100

    FavourStaticEmptyFields: modified expression processing
    Expressions are now processed recursively. This enables
    covering more cases, as well as reduces code duplication.

--- src/FSharpLint.Core/Rules/Conventions/FavourStaticEmptyFields.fs
+++ src/FSharpLint.Core/Rules/Conventions/FavourStaticEmptyFields.fs
@@ -38,30 +38,41 @@ let private generateError (fileContents: string) (range:FSharp.Compiler.Text.Ran
           SuggestedFix = Some suggestedFix
           TypeChecks = List.Empty }
 
-let private runner (args: AstNodeRuleParams) =
-    match args.AstNode with
-    | AstNode.Expression(SynExpr.Const (SynConst.String ("", _, range), _)) -> 
-        generateError args.FileContent range EmptyStringLiteral
-    | AstNode.Expression(SynExpr.ArrayOrList (isArray, [], range)) ->
+[<TailCall>]
+let rec private processExpressions (errorsSoFar: array<WarningDetails>) (args: AstNodeRuleParams) (expressions: list<SynExpr>) =
+    match expressions with
+    | SynExpr.Const(SynConst.String ("", _, range), _) :: tail -> 
+        let errors =
+            Array.append
+                errorsSoFar
+                (generateError args.FileContent range EmptyStringLiteral)
+        processExpressions errors args tail
+    | SynExpr.ArrayOrList (isArray, [], range) :: tail ->
         let emptyLiteralType =
             if isArray then EmptyArrayLiteral else EmptyListLiteral
-        generateError args.FileContent range emptyLiteralType
-    | AstNode.Expression(SynExpr.Record(_, _, synExprRecordField, _)) ->
+        let errors =
+            Array.append
+                errorsSoFar
+                (generateError args.FileContent range emptyLiteralType)
+        processExpressions errors args tail
+    | SynExpr.App(_, _, _, argExpr, _) :: tail ->
+        processExpressions errorsSoFar args (argExpr :: tail)
+    | SynExpr.Record(_, _, synExprRecordFields, _) :: tail ->
         let mapping =
             function
-            | SynExprRecordField(_, _, expr, _) ->
-                match expr with
-                | Some(SynExpr.ArrayOrList(isArray, [], range)) ->
-                    let emptyLiteralType = if isArray then EmptyArrayLiteral else EmptyListLiteral
-                    generateError args.FileContent range emptyLiteralType
-                | Some(SynExpr.Const (SynConst.String ("", _, range), _)) ->
-                    generateError args.FileContent range EmptyStringLiteral
-                | Some(SynExpr.App(_, _, _, SynExpr.Const (SynConst.String ("", _, range), _), _)) ->
-                    generateError args.FileContent range EmptyStringLiteral
-                | _ -> Array.empty
-        synExprRecordField
-        |> List.map mapping
-        |> Array.concat
+            | SynExprRecordField(_, _, expr, _) -> expr
+        let fieldExpressions = 
+            synExprRecordFields
+            |> List.choose mapping
+        processExpressions errorsSoFar args (List.append fieldExpressions tail)
+    | _ :: tail ->
+        processExpressions errorsSoFar args tail
+    | [] -> errorsSoFar
+
+let private runner (args: AstNodeRuleParams) =
+    match args.AstNode with
+    | AstNode.Expression(expr) -> 
+        processExpressions Array.empty args (List.singleton expr)
     | _ -> Array.empty
 
 
