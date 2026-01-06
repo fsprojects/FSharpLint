37ea50e0ee149e9ef7d83f49530aa5856eba6abe
webwarrior-ws <reg@webwarrior.ws>
2026-01-06 10:32:24 +0100

    Core(Tests): add NoAsyncRunSynchronouslyInLibrary
    Rule and tests for it.

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/NoAsyncRunSynchronouslyInLibrary.fs
@@ -0,0 +1,35 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.NoAsyncRunSynchronouslyInLibrary
+
+open NUnit.Framework
+open FSharpLint.Framework.Rules
+open FSharpLint.Rules
+
+[<TestFixture>]
+type TestNoAsyncRunSynchronouslyInLibrary() =
+    inherit FSharpLint.Core.Tests.TestAstNodeRuleBase.TestAstNodeRuleBase(NoAsyncRunSynchronouslyInLibrary.rule)
+
+    [<Test>]
+    member this.``Async.RunSynchronously should not be used in library code``() =
+        this.Parse("""
+module Program
+
+async {
+    return ()
+}
+|> Async.RunSynchronously""")
+
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.``Async.RunSynchronously may be used in code that declares entry point``() =
+        this.Parse("""
+module Program
+
+[<EntryPoint>]
+let main () =
+    async {
+        return ()
+    }
+    |> Async.RunSynchronously""")
+
+        this.AssertNoWarnings()
