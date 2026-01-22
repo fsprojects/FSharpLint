171b7cbba5ee7c1609675902971f4b4863b6c0d3
webwarrior-ws <reg@webwarrior.ws>
2026-01-22 10:51:27 +0100

    PreferStringInterpolationWithSprintf: get rid of mutable state
    That made tests flaky.

--- src/FSharpLint.Core/Rules/Conventions/PreferStringInterpolationWithSprintf.fs
+++ src/FSharpLint.Core/Rules/Conventions/PreferStringInterpolationWithSprintf.fs
@@ -7,30 +7,6 @@ open FSharp.Compiler.Syntax
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
-let mutable moduleIdentifiers = Set.empty
-let mutable letIdentifiers = Set.empty
-
-[<TailCall>]
-let rec isVisible (id: Ident) asts =
-    let isNamedBinding binding =
-        match binding with
-        | SynBinding(_, _, _, _, _, _, _, SynPat.Named(SynIdent.SynIdent(ident, _), _, _, _), _, _, _, _, _) ->
-            id.idText = ident.idText
-        | _ -> false
-
-    match asts with
-    | AstNode.Expression (SynExpr.LetOrUse (_, _, [binding], _, _, _)) :: rest ->
-        isNamedBinding binding || isVisible id rest
-    | _ :: rest -> isVisible id rest
-    | [] -> false
-
-[<TailCall>]
-let rec getTopLevelParent args index =
-    let parents = List.rev (args.GetParents index)
-    match parents with
-    | AstNode.ModuleDeclaration (SynModuleDecl.Let _) :: rest -> rest
-    | _ -> getTopLevelParent args (index + 1)
-
 let runner args =
     let isStringFormat (identifiers: List<Ident>) =
         "String" = identifiers.[0].idText && "Format" = identifiers.[1].idText
@@ -44,43 +20,38 @@ let runner args =
 
     match args.AstNode with
     | AstNode.Expression(SynExpr.App(_, _, SynExpr.LongIdent(_, SynLongIdent(ids, _, _), _, _), paren, range)) when ids.Length = 2 && isStringFormat ids ->
-        let isTopMember (text: string) =
-            moduleIdentifiers.Contains text
         match paren with
-        | SynExpr.Paren(SynExpr.Tuple(_, [SynExpr.Const(SynConst.String(_), _); _], _, _), _, _, _) ->
+        | SynExpr.Paren(SynExpr.Tuple(_, SynExpr.Const(SynConst.String(_), _) :: _args, _, _), _, _, _) ->
             emitViolation range
-        | SynExpr.Paren(SynExpr.Tuple(_, [SynExpr.Ident identifier; _], _, _), _, _, range)  ->
+        | SynExpr.Paren(SynExpr.Tuple(_, SynExpr.Ident(identifier) :: _args, _, _), _, _, _range)  ->
+            let isBindingOfIdentifierToTemplate binding =
+                match binding with
+                | SynBinding(_, _, _, _, _, _, _, SynPat.Named(SynIdent.SynIdent(bindingIdent, _), _, _, _), _, SynExpr.Const(SynConst.String(value, _, _), _), _, _, _) -> 
+                    value.Contains "{0}" && bindingIdent.idText = identifier.idText
+                | _ -> false
 
-            if isTopMember identifier.idText then
+            let hasDeclarationContainingBindingOfIdentifierToTemplate astNode =
+                match astNode with
+                | AstNode.ModuleOrNamespace(SynModuleOrNamespace(_, _, _, declarations, _, _, _, _, _)) ->
+                    declarations
+                    |> List.exists 
+                        (fun decl ->
+                            match decl with
+                            | SynModuleDecl.Let(_, bindings, _) ->
+                                bindings |> List.exists isBindingOfIdentifierToTemplate
+                            | _ -> false)
+                | _ -> false
+            
+            let parents = args.GetParents args.NodeIndex
+            if parents |> List.exists hasDeclarationContainingBindingOfIdentifierToTemplate then
                 emitViolation range
             else
-                if letIdentifiers.Contains identifier.idText && isVisible identifier (getTopLevelParent args 2) then
-                    emitViolation range
-                else
-                    Array.empty
+                Array.empty
         | _ -> Array.empty
-    | AstNode.Binding(SynBinding(_, _, _, _, _, _, _, SynPat.Named(SynIdent.SynIdent(identifier, _), _, _, _), _, SynExpr.Const(SynConst.String(value, _, _), _), _, _, _)) when value.Contains "{0}" ->
-        let parents = args.GetParents 1
-        match parents with
-        | AstNode.ModuleDeclaration(SynModuleDecl.Let _) :: _ ->
-            moduleIdentifiers <- moduleIdentifiers.Add(identifier.idText)
-        | _ -> letIdentifiers <- letIdentifiers.Add(identifier.idText)
-        Array.empty
-    | AstNode.ModuleDeclaration(SynModuleDecl.Let _) ->
-        letIdentifiers <- Set.empty
-        Array.empty
-    | AstNode.ModuleDeclaration(SynModuleDecl.NestedModule _) ->
-        moduleIdentifiers <- Set.empty
-        letIdentifiers <- Set.empty
-        Array.empty
     | _ -> Array.empty
-
-let cleanup () =
-    moduleIdentifiers <- Set.empty
-    letIdentifiers <- Set.empty
 
 let rule =
     { Name = "PreferStringInterpolationWithSprintf"
       Identifier = Identifiers.PreferStringInterpolationWithSprintf
-      RuleConfig = { AstNodeRuleConfig.Runner = runner; Cleanup = cleanup } }
+      RuleConfig = { AstNodeRuleConfig.Runner = runner; Cleanup = ignore } }
     |> AstNodeRule
