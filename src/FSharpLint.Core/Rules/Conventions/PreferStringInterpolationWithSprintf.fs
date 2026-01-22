215c0ac89bb6a0867c90d56300b16400d9c4476b
Andres G. Aragoneses <knocte@gmail.com>
2026-01-22 16:06:47 +0800

    Core/PreferStringInterpolationWithSprintf: fix SelfCheck

--- src/FSharpLint.Core/Rules/Conventions/PreferStringInterpolationWithSprintf.fs
+++ src/FSharpLint.Core/Rules/Conventions/PreferStringInterpolationWithSprintf.fs
@@ -10,54 +10,52 @@ open FSharpLint.Framework.Rules
 let mutable moduleIdentifiers = Set.empty
 let mutable letIdentifiers = Set.empty
 
-let private isStringFormat (identifiers: List<Ident>) =
-    "String" = identifiers.[0].idText && "Format" = identifiers.[1].idText
+[<TailCall>]
+let rec isVisible (id: Ident) asts =
+    let isNamedBinding binding =
+        match binding with
+        | SynBinding(_, _, _, _, _, _, _, SynPat.Named(SynIdent.SynIdent(ident, _), _, _, _), _, _, _, _, _) ->
+            id.idText = ident.idText
+        | _ -> false
 
-let private genereateErrorMessage range =
-    { Range = range
-      Message = Resources.GetString "RulesPreferStringInterpolationWithSprintf"
-      SuggestedFix = None
-      TypeChecks = List.empty }
-    |> Array.singleton
+    match asts with
+    | AstNode.Expression (SynExpr.LetOrUse (_, _, [binding], _, _, _)) :: rest ->
+        isNamedBinding binding || isVisible id rest
+    | _ :: rest -> isVisible id rest
+    | [] -> false
+
+[<TailCall>]
+let rec getTopLevelParent args index =
+    let parents = List.rev (args.GetParents index)
+    match parents with
+    | AstNode.ModuleDeclaration (SynModuleDecl.Let _) :: rest -> rest
+    | _ -> getTopLevelParent args (index + 1)
 
 let runner args =
+    let isStringFormat (identifiers: List<Ident>) =
+        "String" = identifiers.[0].idText && "Format" = identifiers.[1].idText
+
+    let emitViolation range =
+        { Range = range
+          Message = Resources.GetString "RulesPreferStringInterpolationWithSprintf"
+          SuggestedFix = None
+          TypeChecks = List.empty }
+        |> Array.singleton
+
     match args.AstNode with
     | AstNode.Expression(SynExpr.App(_, _, SynExpr.LongIdent(_, SynLongIdent(ids, _, _), _, _), paren, range)) when ids.Length = 2 && isStringFormat ids ->
         let isTopMember (text: string) =
             moduleIdentifiers.Contains text
         match paren with
         | SynExpr.Paren(SynExpr.Tuple(_, [SynExpr.Const(SynConst.String(_), _); _], _, _), _, _, _) ->
-            genereateErrorMessage range
+            emitViolation range
         | SynExpr.Paren(SynExpr.Tuple(_, [SynExpr.Ident identifier; _], _, _), _, _, range)  ->
 
             if isTopMember identifier.idText then
-                genereateErrorMessage range
+                emitViolation range
             else
-                let isNamedBinding binding =
-                    match binding with
-                    | SynBinding(_, _, _, _, _, _, _, SynPat.Named(SynIdent.SynIdent(ident, _), _, _, _), _, _, _, _, _) ->
-                        identifier.idText = ident.idText
-                    | _ -> false
-
-                let isVisible asts =
-                    let rec loop asts =
-                        match asts with
-                        | AstNode.Expression (SynExpr.LetOrUse (_, _, [binding], _, _, _)) :: rest ->
-                            isNamedBinding binding || loop rest
-                        | _ :: rest -> loop rest
-                        | [] -> false
-                    loop asts
-
-                let getTopLevelParent index =
-                    let rec loop index =
-                        let parents = List.rev (args.GetParents index)
-                        match parents with
-                        | AstNode.ModuleDeclaration (SynModuleDecl.Let _) :: rest -> rest
-                        | _ -> loop (index + 1)
-                    loop index
-
-                if letIdentifiers.Contains identifier.idText && isVisible (getTopLevelParent 2) then
-                    genereateErrorMessage range
+                if letIdentifiers.Contains identifier.idText && isVisible identifier (getTopLevelParent args 2) then
+                    emitViolation range
                 else
                     Array.empty
         | _ -> Array.empty
