7eb374991be3dece30441bbbc05e90a96f1b89d9
webwarrior-ws <reg@webwarrior.ws>
2026-01-28 13:04:26 +0100

    SynchronousFunctionNames: added rule

---
+++ src/FSharpLint.Core/Rules/Conventions/SynchronousFunctionNames.fs
@@ -0,0 +1,22 @@
+﻿module FSharpLint.Rules.SynchronousFunctionNames
+
+open System
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+
+let runner (args: AstNodeRuleParams) =
+    failwith "Not yet implemented"
+
+let rule =
+    AstNodeRule
+        {
+            Name = "SynchronousFunctionNames"
+            Identifier = Identifiers.SynchronousFunctionNames
+            RuleConfig =
+                {
+                    AstNodeRuleConfig.Runner = runner
+                    Cleanup = ignore
+                }
+        }
