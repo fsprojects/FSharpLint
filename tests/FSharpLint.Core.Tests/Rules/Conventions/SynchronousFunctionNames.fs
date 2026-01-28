541a99ddb97e33d419de9f4f285a5dc014416adc
webwarrior-ws <reg@webwarrior.ws>
2026-01-28 13:24:13 +0100

    SynchronousFunctionNames: added test

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/SynchronousFunctionNames.fs
@@ -0,0 +1,21 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.SynchronousFunctionNames
+
+open NUnit.Framework
+
+open FSharpLint.Rules
+open FSharpLint.Core.Tests
+
+[<TestFixture>]
+type TestSynchronousFunctionNames() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(SynchronousFunctionNames.rule)
+
+    [<Test>]
+    member this.``Non-asynchronous function named Async* should give violations offering removing Async prefix``() =
+        this.Parse """
+module Foo =
+    let AsyncBar(): int =
+        1
+"""
+
+        Assert.IsTrue this.ErrorsExist
+        StringAssert.Contains("Bar", this.ErrorMsg)
