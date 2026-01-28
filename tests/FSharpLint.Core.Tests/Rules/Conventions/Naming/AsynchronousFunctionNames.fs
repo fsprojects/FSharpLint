22c6ddc77e0770a4201d4e09baaba0932e4ac1f0
webwarrior-ws <reg@webwarrior.ws>
2026-01-28 13:16:37 +0100

    AsynchronousFunctionNames: added test

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/AsynchronousFunctionNames.fs
@@ -0,0 +1,21 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.AsynchronousFunctionNames
+
+open NUnit.Framework
+
+open FSharpLint.Rules
+open FSharpLint.Core.Tests
+
+[<TestFixture>]
+type TestAsynchronousFunctionNames() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(AsynchronousFunctionNames.rule)
+
+    [<Test>]
+    member this.``Function returning Async<'T> should give violations offering adding Async prefix``() =
+        this.Parse """
+module Foo =
+    let Bar(): Async<int> =
+        async { return 1 }
+"""
+
+        Assert.IsTrue this.ErrorsExist
+        StringAssert.Contains("AsyncBar", this.ErrorMsg)
