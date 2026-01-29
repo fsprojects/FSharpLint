3851817a36921f2b1411c609458512e57ea20167
webwarrior-ws <reg@webwarrior.ws>
2026-01-29 09:58:41 +0100

    SynchronousFunctionNames: added more tests

--- tests/FSharpLint.Core.Tests/Rules/Conventions/SynchronousFunctionNames.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/SynchronousFunctionNames.fs
@@ -30,3 +30,62 @@ module Foo =
 
         Assert.IsTrue this.ErrorsExist
         StringAssert.Contains("Bar", this.ErrorMsg)
+
+    [<Test>]
+    member this.``Non-asynchronous nested function named *Async should give violations offering removing Async suffix``() =
+        this.Parse """
+module Foo =
+    let Bar(): int =
+        let BazAsync(): int =
+            0
+        1
+"""
+        
+        Assert.IsTrue this.ErrorsExist
+        StringAssert.Contains("Baz", this.ErrorMsg)
+
+    [<Test>]
+    member this.``Private non-asynchronous function named *Async should give violations offering removing Async suffix``() =
+        this.Parse """
+module Foo =
+    let private AsyncBar(): int =
+        1
+"""
+
+        Assert.IsTrue this.ErrorsExist
+        StringAssert.Contains("Bar", this.ErrorMsg)
+
+    [<Test>]
+    member this.``Internal non-asynchronous function named *Async should give violations offering removing Async suffix``() =
+        this.Parse """
+module Foo =
+    let internal AsyncBar(): int =
+        1
+"""
+
+        Assert.IsTrue this.ErrorsExist
+        StringAssert.Contains("Bar", this.ErrorMsg)
+
+    [<Test>]
+    member this.``Async functions with Async prefix should give no violations``() =
+        this.Parse """
+let AsyncFoo(): Async<int> =
+    async { return 1 }
+let AsyncBar(): Async<unit> =
+    async { return () }
+let AsyncBaz(): Async<unit> =
+    async { do! Async.Sleep(1000) }
+"""
+
+        Assert.IsTrue this.NoErrorsExist
+
+    [<Test>]
+    member this.``Functions that return Task with Async suffix should give no violations``() =
+        this.Parse """
+let FooAsync(): Task =
+    null
+let BarAsync(): Task<int> =
+    null
+"""
+
+        Assert.IsTrue this.NoErrorsExist
