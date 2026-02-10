5e960a26837740846f86838d1e8236a49a662bbb
webwarrior-ws <reg@webwarrior.ws>
2026-02-10 11:34:03 +0100

    SimpleAsyncComplementaryHelpers: fixed rule

--- src/FSharpLint.Core/Rules/Conventions/Naming/SimpleAsyncComplementaryHelpers.fs
+++ src/FSharpLint.Core/Rules/Conventions/Naming/SimpleAsyncComplementaryHelpers.fs
@@ -10,9 +10,8 @@ open FSharp.Compiler.Syntax
 open FSharp.Compiler.Text
 
 type private ReturnType =
-    | Async
-    | AsyncUnit
-    | Task
+    | Async of typeParam: Option<SynType>
+    | Task of typeParam: Option<SynType>
 
 type private Func =
     {
@@ -36,34 +35,38 @@ let runner (args: AstNodeRuleParams) =
             | Some text -> text
             | None -> failwithf "Invalid range: %A" func.Range
 
+        let tryGetTypeParamString (maybeTypeParam: Option<SynType>) =
+            match maybeTypeParam with
+            | Some typeParam -> ExpressionUtilities.tryFindTextOfRange typeParam.Range args.FileContent
+            | None -> None
+
         let message =
             match func.ReturnType with
-            | Async -> 
+            | Async typeParam -> 
                 let newFuncName = funcDefinitionString.Replace(asyncSuffixOrPrefix + func.BaseName, func.BaseName + asyncSuffixOrPrefix)
+                let typeDefString =
+                    match tryGetTypeParamString typeParam with
+                    | Some "unit" -> ": Task"
+                    | Some str -> $": Task<{str}>"
+                    | None -> String.Empty
                 String.Format(
                     Resources.GetString "RulesSimpleAsyncComplementaryHelpersAsync",
                     newFuncName,
-                    String.Empty,
+                    typeDefString,
                     asyncSuffixOrPrefix,
                     func.BaseName,
                     "()"
                 )
-            | AsyncUnit ->
-                let newFuncName = funcDefinitionString.Replace(asyncSuffixOrPrefix + func.BaseName, func.BaseName + asyncSuffixOrPrefix)
-                String.Format(
-                    Resources.GetString "RulesSimpleAsyncComplementaryHelpersAsync",
-                    newFuncName,
-                    ": Task",
-                    asyncSuffixOrPrefix,
-                    func.BaseName,
-                    "()"
-                )
-            | Task ->
+            | Task typeParam ->
                 let newFuncName = funcDefinitionString.Replace(func.BaseName + asyncSuffixOrPrefix, asyncSuffixOrPrefix + func.BaseName)
+                let typeDefString =
+                    match tryGetTypeParamString typeParam with
+                    | Some str -> $": Async<{str}>"
+                    | None -> String.Empty
                 String.Format(
                     Resources.GetString "RulesSimpleAsyncComplementaryHelpersTask",
                     newFuncName,
-                    String.Empty,
+                    typeDefString,
                     func.BaseName,
                     "()"
                 )
@@ -80,40 +83,38 @@ let runner (args: AstNodeRuleParams) =
     let processDeclarations (declarations: list<SynModuleDecl>) =
         let bindings = getBindings List.Empty declarations
 
-        let funcs = 
-            bindings
-            |> List.choose
-                (fun binding ->
-                    match binding with
-                    | SynBinding(_, _, _, _, _, _, _, SynPat.LongIdent(funcIdent, _, _, _, (None | Some(SynAccess.Public _)), _), returnInfo, _, range, _, _) ->
-                        match funcIdent with
-                        | HasAsyncPrefix name ->
-                            let returnType =
-                                match returnInfo with
-                                | Some(SynBindingReturnInfo(SynType.App(SynType.LongIdent(SynLongIdent(_, _, _)), _, [ SynType.LongIdent (SynLongIdent ([ unitIdent ], [], [None])) ], _, _, _, _), _, _, _))
-                                    when unitIdent.idText = "unit" ->
-                                    AsyncUnit
-                                | _ -> Async
+        let tryGetFunction (binding: SynBinding) =
+            match binding with
+            | SynBinding(_, _, _, _, _, _, _, SynPat.LongIdent(funcIdent, _, _, _, (None | Some(SynAccess.Public _)), _), returnInfo, _, _, _, _) ->
+                let returnTypeParam =
+                    match returnInfo with
+                    | Some(SynBindingReturnInfo(SynType.App(SynType.LongIdent(SynLongIdent _), _, [ typeParam ], _, _, _, _), _, _, _)) ->
+                        Some typeParam 
+                    | _ -> None
+                        
+                match funcIdent with
+                | HasAsyncPrefix name ->
+                    Some
+                        { 
+                            BaseName = name.Substring asyncSuffixOrPrefix.Length
+                            Range = binding.RangeOfHeadPattern
+                            ReturnType = Async returnTypeParam
+                        }
+                | HasAsyncSuffix name ->
+                    Some
+                        {
+                            BaseName = name.Substring(0, name.Length - asyncSuffixOrPrefix.Length)
+                            Range = binding.RangeOfHeadPattern
+                            ReturnType = Task returnTypeParam
+                        }
+                | HasNoAsyncPrefixOrSuffix _ ->
+                    None
+            | _ -> None
 
-                            Some
-                                { 
-                                    BaseName = name.Substring asyncSuffixOrPrefix.Length
-                                    Range = range
-                                    ReturnType = returnType
-                                }
-                        | HasAsyncSuffix name ->
-                            Some
-                                {
-                                    BaseName = name.Substring(0, name.Length - asyncSuffixOrPrefix.Length)
-                                    Range = range
-                                    ReturnType = Task
-                                }
-                        | HasNoAsyncPrefixOrSuffix _ ->
-                            None
-                    | _ -> None)
+        let funcs = bindings |> List.choose tryGetFunction
 
-        let asyncFuncs = funcs |> List.filter (fun func -> func.ReturnType <> Task)
-        let taskFuncs = funcs |> List.filter (fun func -> func.ReturnType = Task)
+        let asyncFuncs = funcs |> List.filter (fun func -> func.ReturnType.IsAsync)
+        let taskFuncs = funcs |> List.filter (fun func -> func.ReturnType.IsTask)
         
         let checkFuncs (targetFuncs: list<Func>) (otherFuncs: list<Func>) =
             targetFuncs
