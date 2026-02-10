d82f1a58cc62fbcf7cdfb1dc3e9b33f4830d8025
webwarrior-ws <reg@webwarrior.ws>
2026-02-10 10:30:18 +0100

    Core: moved library heuristics
    From NoAsyncRunSynchronouslyInLibrary to new module
    Rules.Utilities.LibraryHeuristics, becuase it will also be used
    by SimpleAsyncComplementaryHelpers rule.
    
    Also had to move NamingHelper.fs to parent dir (Rules) because
    it's used by LibraryHeuristics and must appear before
    Utilities.fs file.

--- src/FSharpLint.Core/Rules/Smells/NoAsyncRunSynchronouslyInLibrary.fs
+++ src/FSharpLint.Core/Rules/Smells/NoAsyncRunSynchronouslyInLibrary.fs
@@ -9,11 +9,7 @@ open FSharpLint.Framework.Suggestion
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 open FSharpLint.Framework.Utilities
-
-type LibraryHeuristicResultByProjectName =
-    | Likely
-    | Unlikely
-    | Uncertain
+open FSharpLint.Rules.Utilities.LibraryHeuristics
 
 let hasEntryPoint (checkFileResults: FSharpCheckFileResults) (maybeProjectCheckResults: Option<FSharpCheckProjectResults>) =
     let hasEntryPointInTheSameFile =
@@ -29,48 +25,6 @@ let hasEntryPoint (checkFileResults: FSharpCheckFileResults) (maybeProjectCheckR
         |> Seq.exists (fun implFile -> implFile.HasExplicitEntryPoint)
     | None ->
         false
-
-let private projectNamesUnlikelyToBeLibraries =
-    [
-        "tests"
-        "test"
-        "testing"
-        "console"
-        "CLI"
-        "TUI"
-    ]
-    |> Seq.map (fun name -> name.ToLowerInvariant())
-
-let private possibleProjectNameSegmentSeparators =
-    [|
-        '.'
-        '_'
-        '-'
-    |]
-
-let howLikelyProjectIsLibrary (projectFileName: string): LibraryHeuristicResultByProjectName =
-    let libraryAbbrev = "lib"
-    let nameSegments =
-        Helper.Naming.QuickFixes.splitByCaseChange projectFileName
-        |> Seq.map (fun segment -> segment.ToLowerInvariant())
-    if nameSegments |> Seq.contains libraryAbbrev then
-        Likely
-    elif
-        nameSegments
-        |> Seq.exists (
-            fun segment ->
-                let subSegments = segment.Split possibleProjectNameSegmentSeparators
-                subSegments
-                |> Seq.exists (fun subSegment ->
-                    projectNamesUnlikelyToBeLibraries
-                    |> Seq.exists (fun noLibName -> noLibName = subSegment)
-                )
-        ) then
-        Unlikely
-    elif projectFileName.ToLowerInvariant().EndsWith libraryAbbrev then
-        Likely
-    else
-        Uncertain
 
 let extractAttributeNames (attributes: SynAttributes) =
     seq {
