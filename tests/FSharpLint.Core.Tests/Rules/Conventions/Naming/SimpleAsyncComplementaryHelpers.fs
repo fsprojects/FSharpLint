75bb6067082dededde99858a26e7e67b9fa66114
webwarrior-ws <reg@webwarrior.ws>
2026-02-10 11:35:15 +0100

    SimpleAsyncComplementaryHelpers: added another test

--- tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/SimpleAsyncComplementaryHelpers.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/SimpleAsyncComplementaryHelpers.fs
@@ -124,3 +124,15 @@ module Foo =
         Assert.IsTrue this.ErrorsExist
         StringAssert.Contains("BarAsync(foo: int): Task<int>", this.ErrorMsg)
         StringAssert.Contains("Async.StartAsTask(AsyncBar foo)", this.ErrorMsg)
+
+    [<Test>]
+    member this.``Function parameters in tuple form must be preserved in offered solution``() =
+        this.Parse """
+module Foo =
+    let AsyncBar(foo: int, bar): Async<int> =
+        async { return 0 }
+"""
+
+        Assert.IsTrue this.ErrorsExist
+        StringAssert.Contains("BarAsync(foo: int, bar): Task<int>", this.ErrorMsg)
+        StringAssert.Contains("Async.StartAsTask(AsyncBar(foo, bar))", this.ErrorMsg)
