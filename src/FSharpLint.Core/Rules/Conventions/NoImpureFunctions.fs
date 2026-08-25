354f5e6f5f9de72a8ae210331165a16cb081607e
webwarrior-ws <reg@webwarrior.ws>
2026-08-25 14:16:58 +0200

    NoImpureFunctions: implemented rule

--- src/FSharpLint.Core/Rules/Conventions/NoImpureFunctions.fs
+++ src/FSharpLint.Core/Rules/Conventions/NoImpureFunctions.fs
@@ -1,6 +1,10 @@
 ﻿module FSharpLint.Rules.NoImpureFunctions
 
 open System
+open FSharp.Compiler.Text
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
 [<RequireQualifiedAccess>]
@@ -9,8 +13,58 @@ type Config = {
     AdditionalImpureFunctions:string list
 }
 
-let runner (_config:Config) (_args:AstNodeRuleParams) =
-    failwith "Not yet implemented"
+let private impureFunctionIdentifiers =
+    Map.ofList
+        [
+            ("Array.sortInPlace", Some "Array.sort")
+            ("Array.sortInPlaceBy", Some "Array.Array.sortBy")
+            ("Array.sortInPlaceWith", Some "Array.sortWith")
+
+            ("Array.set", Some "Array.updateAt")
+
+            ("Array.fill", None)
+            ("Array.blit", None)
+
+            ("Array2D.fill", None)
+            ("Array2D.blit", None)
+        ]
+
+let runner (config:Config) (args:AstNodeRuleParams) =
+    let checkIfImpureIdentifier (identifier:string) (range:Range) =
+        let issueWarning maybeReplacementFunction =
+            match maybeReplacementFunction with
+            | Some replacementFunction ->
+                {
+                    Range = range
+                    Message = String.Format(Resources.GetString "RulesConventionsNoImpureFunctionsReplacementError", replacementFunction, identifier)
+                    SuggestedFix = Some (lazy ( Some { FromText = identifier; FromRange = range; ToText = replacementFunction }))
+                    TypeChecks = List.Empty
+                }
+            | None -> 
+                {
+                    Range = range
+                    Message = String.Format(Resources.GetString "RulesConventionsNoImpureFunctionsError", identifier)
+                    SuggestedFix = None
+                    TypeChecks = List.Empty
+                }
+
+        if List.contains identifier config.AllowedImpureFunctions then
+            None
+        elif List.contains identifier config.AdditionalImpureFunctions then
+            issueWarning None |> Some
+        else
+            Map.tryFind identifier impureFunctionIdentifiers
+            |> Option.filter (fun _ -> not (List.contains identifier config.AllowedImpureFunctions))
+            |> Option.map issueWarning
+
+    match args.AstNode with
+    | AstNode.Identifier (identifier, range) ->
+        match checkIfImpureIdentifier (String.concat "." identifier) range with
+        | Some impureIdentifierWarning ->
+            Array.singleton impureIdentifierWarning
+        | _ ->
+            Array.Empty()
+    | _ -> Array.empty
 
 let rule config =
     AstNodeRule
@@ -23,4 +77,3 @@ let rule config =
                     Cleanup = ignore
                 }
         }
-
